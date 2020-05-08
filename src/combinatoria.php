a18941d9e6903063da3ec632555f550344d9ad2b
rudymartinb <paraespam541@gmail.com>
2020-05-08 20:44:36 -0300

    autocommit por pruebas sin fallar

--- src/combinatoria.php
+++ src/combinatoria.php
@@ -47,10 +47,8 @@ class Combinatoria {
             return;
         }
         
-        // 1 2
-        $this->swap120( $funcion );
-        // 3 4
-        $this->swap120( $funcion );
+        $this->swap120( $funcion ); // 1 2
+        $this->swap120( $funcion ); // 3 4
         
         if( count( $this->array ) == 3 ){
             return;
@@ -67,6 +65,11 @@ class Combinatoria {
 
         $this->swap( 2, 3 );
         $funcion( $this->array );
+
+        $this->swap120( $funcion ); // 6 7
+        $this->swap120( $funcion ); // 8 9
+        $this->swap120( $funcion ); // 10 11
+        
 //         $funcion( $this->array );
         
 //         $this->swap120( $funcion );
