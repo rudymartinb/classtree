80510b8d8d3aeb3e8bb443b92aa64bd99526dbee
rudymartinb <paraespam541@gmail.com>
2020-05-08 19:28:51 -0300

    autocommit por pruebas sin fallar

--- tests/combinatoriaTest.php
+++ tests/combinatoriaTest.php
@@ -83,5 +83,43 @@ class combinatoriaTest extends PHPUnit\Framework\TestCase {
         $this->assertEquals( 3*2*1, $cual );
         
     }
+
+//     function test_4(){
+//         $comb = new Combinatoria();
+//         $arr = [ "A", "B", "C", "D" ];
+//         $comb->set_array( $arr );
+//         $cual = 0;
+        
+//         $comb->set_funcion(
+//             function( $elemento ) use ( & $cual ){
+//                 switch( $cual ){
+//                     case 0:
+//                         $this->assertEquals( ["A","B", "C","D"], $elemento );
+//                         break;
+//                     case 1:
+//                         $this->assertEquals( ["B","A","C","D"], $elemento );
+//                         break;
+//                     case 2:
+//                         $this->assertEquals( ["A","C","B","D"], $elemento );
+//                         break;
+//                     case 3:
+//                         $this->assertEquals( ["C","A","B","D"], $elemento );
+//                         break;
+//                     case 4:
+//                         $this->assertEquals( ["B","C","A","D"], $elemento );
+//                         break;
+//                     case 5:
+//                         $this->assertEquals( ["C","B","A","D"], $elemento );
+//                         break;
+//                 }
+//                 $cual++;
+//             }
+//             );
+        
+//         $comb->ejecutar();
+        
+//         $this->assertEquals( 4*3*2*1, $cual );
+        
+//     }
     
 }
