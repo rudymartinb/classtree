f6efa0c271cacff4ab17d4ec73fd34b91a642d7d
rudymartinb <paraespam541@gmail.com>
2020-05-08 19:05:28 -0300

    autocommit por pruebas sin fallar

--- src/combinatoria.php
+++ src/combinatoria.php
@@ -44,7 +44,6 @@ class Combinatoria {
         $funcion( $this->array );
         
         $this->swap( 0, 1 );
-//         $this->swap( 0, 2 );
         $funcion( $this->array );
         
         
