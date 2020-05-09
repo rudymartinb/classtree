573fc566a07bf1602499e19694b30f3dcebfd159
rudymartinb <paraespam541@gmail.com>
2020-05-09 04:13:10 -0300

    autocommit por pruebas sin fallar

--- tests/combinatoriaTest.php
+++ tests/combinatoriaTest.php
@@ -29,7 +29,7 @@ class combinatoriaTest extends PHPUnit\Framework\TestCase {
     function test_splice() {
         $arr = [ "A", "B", "C" ];
         $copia = $arr;
-        $cabeza = array_splice( $copia, 1,1 );
+        $cabeza = array_splice( $copia, 0,1 );
         $copia = $arr;
         $resto = array_slice( $copia, 1,1 );
         vaR_dump( $cabeza );
