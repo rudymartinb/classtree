98bfe77a43a36cd82fdb916ffa660f0154473f61
rudymartinb <paraespam541@gmail.com>
2020-05-10 03:36:16 -0300

    autocommit por pruebas sin fallar

--- src/tree.php
+++ src/tree.php
@@ -9,19 +9,19 @@ namespace nodes;
  * so I need to be able to have them all at hand to process them
  */
 class tree {
-    private $lista = [];
+    private $nodelist = [];
     function add_node( node &$nodo ){
-        $this->lista[] = & $nodo;
+        $this->nodelist[] = & $nodo;
     }
     
     function get_num_nodes() : int {
-        return count( $this->lista );
+        return count( $this->nodelist );
     }
     
     
     function get_num_orphans() : int {
         $orphans = 0;
-        foreach ($this->lista as $nodo ) {
+        foreach ($this->nodelist as $nodo ) {
             if( $nodo->get_num_parents() == 0 ){
                 $orphans ++;
             }
