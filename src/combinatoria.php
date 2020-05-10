b8a2f2b026b8cf6bd8a9e0a87e325157f3d8a785
rudymartinb <paraespam541@gmail.com>
2020-05-09 22:41:29 -0300

    autocommit por pruebas sin fallar

--- src/combinatoria.php
+++ src/combinatoria.php
@@ -2,19 +2,20 @@
 
 /* extract the second item and place it as first
  * then concatenates the rest of the array
+ * 
  * cabeza = head
  * resto = remaining items
  * resultado = result
  * 
  */
-
 function permutacion( Array $arr, int $ini ) : Array {
     $resto = $arr;
     $cabeza = array_splice( $resto, $ini,1 );
     $resultado = array_merge( $cabeza , $resto );
     return $resultado;
-    
 }
+
+
 function generar_permutaciones( Array $arr = null, Array $cabecera = [] ) : Array {
     if( is_null( $arr ) or count( $arr ) < 2 ){
         return [ $arr ];
@@ -34,9 +35,8 @@ function generar_permutaciones( Array $arr = null, Array $cabecera = [] ) : Arra
             $resto = $arr;
             array_splice( $resto, $index, 1 );
             $partes = generar_permutaciones( $resto, array_merge( $cabecera, $cabeza ) );
-            
             foreach( $partes as $value ){
-                $resultado[] = $value ; // array_merge( $cabeza, $value );
+                $resultado[] = $value;
             }
         }
         
