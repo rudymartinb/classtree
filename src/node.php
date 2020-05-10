f3274dcddfb5640026499677c85a8203e745904d
rudymartinb <paraespam541@gmail.com>
2020-05-10 03:33:28 -0300

    autocommit por pruebas sin fallar

--- src/node.php
+++ src/node.php
@@ -7,28 +7,7 @@ abstract class node {
     abstract function get_level() : int;
 }
 
-class tree {
-    private $lista = [];
-    function add_node( node &$nodo ){
-        $this->lista[] = & $nodo;
-    }
-    
-    function get_num_nodes() : int {
-        return count( $this->lista );
-    }
-    
 
-    function get_num_orphans() : int {
-        $orphans = 0;
-        foreach ($this->lista as $nodo ) {
-            if( $nodo->get_num_parents() == 0 ){
-                $orphans ++;
-            }
-        }
-        return $orphans;
-    }
-    
-}
 
 class node_clase extends node {
     
--- src/tree.php
+++ src/tree.php
@@ -1,1 +1,24 @@
 <?php
+namespace nodes;
+class tree {
+    private $lista = [];
+    function add_node( node &$nodo ){
+        $this->lista[] = & $nodo;
+    }
+    
+    function get_num_nodes() : int {
+        return count( $this->lista );
+    }
+    
+    
+    function get_num_orphans() : int {
+        $orphans = 0;
+        foreach ($this->lista as $nodo ) {
+            if( $nodo->get_num_parents() == 0 ){
+                $orphans ++;
+            }
+        }
+        return $orphans;
+    }
+    
+}
