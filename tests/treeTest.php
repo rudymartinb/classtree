8ba6defc18c4564c296e9606d6f18145e246d416
rudymartinb <paraespam541@gmail.com>
2020-05-10 02:04:00 -0300

    autocommit por pruebas sin fallar

--- tests/treeTest.php
+++ tests/treeTest.php
@@ -1,6 +1,46 @@
 <?php
 
 class treeTest extends PHPUnit\Framework\TestCase {
+    /* builder
+     */
+    function crear_1nodo() : tree {
+        $nodo = new nodo_clase();
+        $nodo->set_clase( new clase("father") );
+        
+        $tree = new tree();
+        $tree->add_nodo( $nodo );
+        return $tree;
+    }
+    
+    function test_nodo_clase() {
+        $tree = $this->crear_1nodo();
+        $this->assertEquals( 1, $tree->get_num_nodes() );
+    }
+    
+    function crear_2nodos() : tree {
+        $nodo = new nodo_clase();
+        $nodo->set_clase( new clase("father") );
+        
+        $tree = new tree();
+        $tree->add_nodo( $nodo );
+        
+        $nodo = new nodo_clase();
+        $nodo->set_clase( new clase("mother") );
+        $tree->add_nodo( $nodo );
+        return $tree;
+    }
+    
+    function test_nodo_2clases() {
+        $tree = $this->crear_2nodos();
+        
+        $this->assertEquals( 2, $tree->get_num_nodes() );
+    }
+    
+    function test_tree_orphans_0() {
+        $tree = new tree();
+        
+        $this->assertEquals( 0, $tree->get_num_orphans() );
+    }
     
        
 }
