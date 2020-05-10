d3aeb3b1e68811216251b2ad6ebba414bf7df941
rudymartinb <paraespam541@gmail.com>
2020-05-10 02:02:35 -0300

    autocommit por pruebas sin fallar

---
+++ tests/treeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+/**
+ *  test case.
+ */
+class treeTest extends PHPUnit_Framework_TestCase
+{
+	
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		// TODO Auto-generated treeTest::setUp()
+		
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		// TODO Auto-generated treeTest::tearDown()
+		
+		parent::tearDown();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+		// TODO Auto-generated constructor
+	}
+
+	
+}
+
