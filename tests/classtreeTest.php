9f2652881432a9fc624568a9edc8789ecb2910ab
rudymartinb <paraespam541@gmail.com>
2020-05-10 06:54:48 -0300

    autocommit por pruebas sin fallar

--- tests/classtreeTest.php
+++ tests/classtreeTest.php
@@ -22,7 +22,7 @@ function get_all_files( string $path ) : Array {
 class classtreeTest extends PHPUnit\Framework\TestCase {
     
     function test_get_files() {
-        $path = "/home/rudy/projects/classtree/tests/dummy/";
+        $path = "/home/rudy/projects/classtree/tests/dummy";
         $lista = get_all_files( $path );
         $this->assertEquals( 3, count( $lista ) );
         
