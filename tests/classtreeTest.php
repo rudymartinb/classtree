ce623cd3179e861d44166444df8dd3abbb1897b1
rudymartinb <paraespam541@gmail.com>
2020-05-11 22:17:54 -0300

    autocommit por pruebas sin fallar

--- tests/classtreeTest.php
+++ tests/classtreeTest.php
@@ -72,7 +72,7 @@ class classtreeTest extends PHPUnit\Framework\TestCase {
         $diagram->addClasses( $classes );
         $diagram->resolve_dependencies();
         
-        $this->assertTrue( $diagram->is_dependencies_resolved(), "una clase solitaria deberia tener las dependencias resueltas" );
+        $this->assertTrue( $diagram->is_dependencies_resolved(), "??" );
     }
     
 
