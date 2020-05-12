6903a65d5930cba933d256ff9ad651b604e1f251
rudymartinb <paraespam541@gmail.com>
2020-05-11 22:21:57 -0300

    autocommit por pruebas sin fallar

--- tests/classtreeTest.php
+++ tests/classtreeTest.php
@@ -64,16 +64,10 @@ class classtreeTest extends PHPUnit\Framework\TestCase {
         $classes = get_clases( $source );
         return $classes;
     }
-    function generate_2_separated_classes() : Array {
-        $source = $this->get_source_prueba();
-        $classes = get_clases( $source );
-        
+    function generate_1_separated_classes() : Array {
         $source2 = $this->get_source_prueba2_2();
         $classes2 = get_clases( $source2 );
-        
-        $classes = array_merge( $classes, $classes2 );
-        
-        return $classes;
+        return $classes2;
     }
     
     
@@ -88,14 +82,14 @@ class classtreeTest extends PHPUnit\Framework\TestCase {
         $this->assertTrue( $diagram->is_dependencies_resolved(), "??" );
     }
 
-    function test_resolve_dependencies_2_separated_classes() {
-        $classes = $this->generate_2_separated_classes();
+    function test_resolve_dependencies_1_separated_classes() {
+        $classes = $this->generate_1_separated_classes();
         
         $diagram = new ClassDiagram();
         $diagram->addClasses( $classes );
-//         $diagram->resolve_dependencies();
+        $diagram->resolve_dependencies();
         
-        $this->assertFalse( $diagram->is_dependencies_resolved(), "??" );
+        $this->assertTrue( $diagram->is_dependencies_resolved(), "??" );
     }
     
 
