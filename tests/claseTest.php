18c56279a11eba48e4da281bf449687bcea63679
rudymartinb <paraespam541@gmail.com>
2020-05-12 00:28:25 -0300

    autocommit por pruebas sin fallar

---
+++ tests/claseTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+/**
+ *  test case.
+ */
+class claseTest extends PHPUnit_Framework_TestCase
+{
+	
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		// TODO Auto-generated claseTest::setUp()
+		
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		// TODO Auto-generated claseTest::tearDown()
+		
+		parent::tearDown();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+		// TODO Auto-generated constructor
+	}
+
+	
+}
+
