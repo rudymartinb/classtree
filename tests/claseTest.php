484cd7f1f8bb73859028309a7213f189a8cbc1fe
rudymartinb <paraespam541@gmail.com>
2020-05-14 00:29:07 -0300

    autocommit por pruebas sin fallar

--- tests/claseTest.php
+++ tests/claseTest.php
@@ -17,60 +17,6 @@ class claseTest extends PHPUnit\Framework\TestCase {
         
     }
 
-    function test_is_resolved(){
-        $diagram = new ClassDiagram();
-        
-        $class = new clase("parent");
-        $diagram->addClass( $class );
-        
-        
-        $class2 = new clase("child");
-        $diagram->addClass( $class2 );
-        $class2->set_extends( "parent" );
-        
-        $class2->find_parent( $diagram->get_classes() );
-        
-        $this->assertTrue( $class2->is_parent_resolved(), "subclass is now linked with parent");
-    }
-    
-    function test_is_resolved_and_same_obj(){
-        $diagram = new ClassDiagram();
-        
-        $class = new clase("parent");
-        $diagram->addClass( $class );
-
-        $isacopy = $class;
-        $fake = new clase("parent");
-
-        
-        
-        $class2 = new clase("child");
-        $diagram->addClass( $class2 );
-        $class2->set_extends( "parent" );
-        
-        $class2->find_parent( $diagram->get_classes() );
-        
-        $this->assertTrue( $class2->get_parent() === $class, "subclass is now linked with parent");
-        $this->assertTrue( $class2->get_parent() === $isacopy, "subclass shoudl be linked with a copy of parent");
-        $this->assertFalse( $class2->get_parent() === $fake, "subclass is now linked with parent");
-    }
-    
-    
-    function test_is_NOT_resolved_2(){
-        $diagram = new ClassDiagram();
-        
-//         $class = new clase("parent");
-//         $diagram->addClass( $class );
-        
-        
-        $class2 = new clase("child");
-        $diagram->addClass( $class2 );
-        $class2->set_extends( "parent" );
-        
-        $class2->find_parent( $diagram->get_classes() );
-        
-        $this->assertFalse( $class2->is_parent_resolved(), "subclass is now linked with parent");
-    }
     
     
 }
