5e0be11f2473d8c6a2e83d2318626df24551cb2c
rudymartinb <paraespam541@gmail.com>
2020-05-13 21:43:07 -0300

    autocommit por pruebas sin fallar

--- tests/mocks/sources.php
+++ tests/mocks/sources.php
@@ -1,1 +1,73 @@
 <?php
+
+function get_source_prueba() : string {
+    $source = '<?php
+/*
+ * codigo de ejemplo usado para alimentar las pruebas unitarias
+ *
+ * una vez terminado NO TOCAR !!!
+ */
+namespace something;
+        
+use DateTime;
+        
+interface sarasa_interface {
+    function algo() : string;
+        
+}
+class father {
+    function algo1( int $uno, string $dos ): string {
+        
+    }
+        
+    function algo2( int $uno, string $dos ) {
+    }
+    function algo3( ) : bool {
+    }
+    function algo4( ) {
+    }
+}
+        
+class son extends father implements sarasa_interface {
+    public function algo(): string {
+        
+    }
+    public function algomas(): DateTime {
+        
+    }
+}
+        
+class orphan implements sarasa_interface {
+    public function algo() : string {
+        
+    }
+}
+';
+    return $source;
+}
+
+
+
+function get_source_prueba2() : string {
+    $source = "<?php
+namespace sarasa;
+        
+class father {
+    public function algo(): string {
+        
+    }
+}";
+    return $source;
+}
+
+function get_source_prueba2_2() : string {
+    $source = "<?php
+namespace sarasa;
+        
+class son extends father {
+    public function algo(): string {
+        
+    }
+}";
+    return $source;
+}
