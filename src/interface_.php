0a5079b4268645ef06077d5682321b43f97b46a7
rudymartinb <paraespam541@gmail.com>
2020-05-14 22:30:25 -0300

    autocommit por pruebas sin fallar

--- src/interface_.php
+++ src/interface_.php
@@ -2,7 +2,12 @@
 namespace src;
 
 class interface_ {
+    private $name;
     function __construct( string $name ) {
-        
+        $this->name = $name;
+    }
+    
+    function get_name() : string {
+        return $this->name;
     }
 }
--- tests/interfaceTest.php
+++ tests/interfaceTest.php
@@ -7,6 +7,7 @@ class interfaceTest extends PHPUnit\Framework\TestCase {
     function test_new(){
         $interface = new interface_( "something_interface" );
         
+        $this->assertEquals("something_interface", $interface->get_name() );
         
     }
 }
