2a4e2a2ebc64782713eef1ce63eae16cd56a28f1
rudymartinb <paraespam541@gmail.com>
2020-05-14 22:22:43 -0300

    autocommit por pruebas sin fallar

--- tests/interfaceTest.php
+++ tests/interfaceTest.php
@@ -1,36 +1,6 @@
 <?php
 
-/**
- *  test case.
- */
-class interfaceTest extends PHPUnit_Framework_TestCase
-{
-
-    /**
-     * Prepares the environment before running a test.
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        // TODO Auto-generated interfaceTest::setUp()
-    }
-
-    /**
-     * Cleans up the environment after running a test.
-     */
-    protected function tearDown()
-    {
-        // TODO Auto-generated interfaceTest::tearDown()
-        parent::tearDown();
-    }
-
-    /**
-     * Constructs the test case.
-     */
-    public function __construct()
-    {
-        // TODO Auto-generated constructor
-    }
+class interfaceTest extends PHPUnit\Framework\TestCase {
+    
 }
 
