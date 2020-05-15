9d277889bded3fb45b84375f7d53f2dd965fcdab
rudymartinb <paraespam541@gmail.com>
2020-05-15 01:52:50 -0300

    autocommit por pruebas sin fallar

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,36 +1,10 @@
 <?php
 
-/**
- *  test case.
- */
-class AppTest extends PHPUnit_Framework_TestCase
-{
-
-    /**
-     * Prepares the environment before running a test.
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        // TODO Auto-generated AppTest::setUp()
-    }
-
-    /**
-     * Cleans up the environment after running a test.
-     */
-    protected function tearDown()
-    {
-        // TODO Auto-generated AppTest::tearDown()
-        parent::tearDown();
-    }
-
-    /**
-     * Constructs the test case.
-     */
-    public function __construct()
-    {
-        // TODO Auto-generated constructor
+class AppTest extends PHPUnit\Framework\TestCase {
+    function test_new(){
+        $app = new App();
+        
+        
     }
 }
 
