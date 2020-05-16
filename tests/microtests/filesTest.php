342b27abbf3f67305c27ee22af440117875ea14c
rudymartinb <paraespam541@gmail.com>
2020-05-16 17:17:29 -0300

    autocommit por pruebas sin fallar

--- tests/microtests/filesTest.php
+++ tests/microtests/filesTest.php
@@ -119,6 +119,15 @@ class filesTest extends PHPUnit\Framework\TestCase {
         $interfaces = get_interfaces_from_sources( $sources );
         $this->assertEquals( 1, count( $interfaces ) );
     }
+
+    function test_create_cree() {
+        $path = "./tests/dummy";
+        $files = get_all_files( $path );
+        $php_filenames = get_php_files( $files );
+        $sources = get_sources( $php_filenames );
+        $classes = get_classes_from_sources( $sources );
+        $this->assertEquals( 4, count( $classes ) );
+    }
     
     
     
