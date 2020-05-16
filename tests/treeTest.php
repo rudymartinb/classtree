5a3607bc1e0336edd86e6b3980d5f84cad8e1340
rudymartinb <paraespam541@gmail.com>
2020-05-16 17:24:24 -0300

    autocommit por pruebas sin fallar

---
+++ tests/treeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class treeTest extends PHPUnit\Framework\TestCase {
+    function test_tree_empty(){
+        $classes = [];
+        $actual = get_tree( $classes );
+        $this->assertEquals( 0, count( $actual ) == 0 );
+    }
+    
+       
+}
+
