5747837515dc7cdca6945a7755f5fd2632f66108
rudymartinb <paraespam541@gmail.com>
2020-05-16 20:27:17 -0300

    autocommit por pruebas sin fallar

--- tests/treeTest.php
+++ tests/treeTest.php
@@ -177,36 +177,36 @@ class treeTest extends PHPUnit\Framework\TestCase {
         $this->assertEquals( 3, $actual );
     }
 
-//     function test_get_max_width_4_parents_3_children(){
-//         $classes = [];
-//         $class = new class_("parent");
-//         $classes[] = $class ;
+    function test_get_max_width_4_parents_3_children(){
+        $classes = [];
+        $class = new class_("parent");
+        $classes[] = $class ;
         
-//         $class2 = new class_("child");
-//         $class2->set_extends("parent");
-//         $classes[] = $class2;
+        $class2 = new class_("child");
+        $class2->set_extends("parent");
+        $classes[] = $class2;
         
-//         $class3 = new class_("child2");
-//         $class3->set_extends("parent");
-//         $classes[] = $class3;
+        $class3 = new class_("child2");
+        $class3->set_extends("parent");
+        $classes[] = $class3;
         
-//         $class4 = new class_("child3");
-//         $class4->set_extends("parent");
-//         $classes[] = $class3;
+        $class4 = new class_("child3");
+        $class4->set_extends("parent");
+        $classes[] = $class3;
         
-//         $class = new class_("parent1");
-//         $classes[] = $class ;
-//         $class = new class_("parent2");
-//         $classes[] = $class ;
-//         $class = new class_("parent3");
-//         $classes[] = $class ;
+        $class = new class_("parent1");
+        $classes[] = $class ;
+        $class = new class_("parent2");
+        $classes[] = $class ;
+        $class = new class_("parent3");
+        $classes[] = $class ;
         
-//         $tree = get_tree( $classes );
-// //         var_dump( $tree );
-//         $actual = get_max_width( $tree );
+        $tree = get_tree( $classes );
+//         var_dump( $tree );
+        $actual = get_max_width( $tree );
         
-//         $this->assertEquals( 6, $actual );
-//     }
+        $this->assertEquals( 6, $actual );
+    }
     
 //     function test_get_max_width_100_classes(){
 //         $classes = [];
