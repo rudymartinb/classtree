ffc849a477cfb5c12096e2290955caafb29bf66c
rudymartinb <paraespam541@gmail.com>
2020-05-16 18:51:17 -0300

    autocommit por pruebas sin fallar

--- tests/treeTest.php
+++ tests/treeTest.php
@@ -112,18 +112,31 @@ class treeTest extends PHPUnit\Framework\TestCase {
         $classes = [];
         $class = new class_("parent");
         $classes[] = $class ;
+    
+        $tree = get_tree( $classes );
         
-//         $class2 = new class_("child");
-//         $class2->set_extends("parent");
-//         $classes[] = $class2;
+        $actual = get_max_width( $tree );
+        //         var_dump($actual);
         
-//         $class3 = new class_("child2");
-//         $class3->set_extends("parent");
-//         $classes[] = $class3;
+        $this->assertEquals( 1, $actual );
+    }
 
-//         $class4 = new class_("child3");
-//         $class4->set_extends("parent");
-//         $classes[] = $class3;
+    function test_get_max_width_1_1(){
+        $classes = [];
+        $class = new class_("parent");
+        $classes[] = $class ;
+        
+        $class2 = new class_("child");
+        $class2->set_extends("parent");
+        $classes[] = $class2;
+        
+        //         $class3 = new class_("child2");
+        //         $class3->set_extends("parent");
+        //         $classes[] = $class3;
+        
+        //         $class4 = new class_("child3");
+        //         $class4->set_extends("parent");
+        //         $classes[] = $class3;
         
         
         $tree = get_tree( $classes );
