e4889da743942c2128f9a5b23a6417129af08c35
rudymartinb <paraespam541@gmail.com>
2020-05-16 17:41:31 -0300

    autocommit por pruebas sin fallar

--- tests/treeTest.php
+++ tests/treeTest.php
@@ -1,11 +1,20 @@
 <?php
 
 use src\class_;
+use function src\force_class;
 
-function get_tree( Array $classes ){
+function get_tree( Array $classes, string $parent = "" ){
     $result = [];
     foreach( $classes as $class ){
-        $result[] = [ $class, [] ] ;
+        if( $parent != "" ){
+            $class = force_class($class);
+            if( $class->get_extends() != $parent ){
+                continue;
+            }
+        }
+        if( $class->get_extends() == ""){
+            $result[] = [ $class, get_tree( $classes, $class->get_name() ) ] ;
+        }
     }
     return  $result;
 }
@@ -38,6 +47,24 @@ class treeTest extends PHPUnit\Framework\TestCase {
         
         $this->assertEquals( 2, count( $actual[0] ) );
         $this->assertEquals( 2, count( $actual[1] ) );
+    }
+
+    function test_tree_parent_child(){
+        $classes = [];
+        $class = new class_("parent");
+        $classes[] = $class ;
+        $class2 = new class_("child");
+        $class2->set_extends("parent");
+        $classes[] = $class2;
+        
+        $actual = get_tree( $classes );
+        
+        var_dump($actual);
+        // theres only one three in the first element
+        $this->assertEquals( 1, count( $actual ) );
+        
+//         $this->assertEquals( 2, count( $actual[0] ) );
+//         $this->assertEquals( 2, count( $actual[1] ) );
     }
     
     
