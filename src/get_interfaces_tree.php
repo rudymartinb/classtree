d0260303db7dfad2d8e5119d8c48b9ec7311cd90
rudymartinb <paraespam541@gmail.com>
2020-05-17 00:47:24 -0300

    autocommit por pruebas sin fallar

--- src/get_interfaces_tree.php
+++ src/get_interfaces_tree.php
@@ -1,5 +1,5 @@
 <?php
 
 function get_interfaces_tree(){
-    
+    return [];
 }
--- tests/interfacesTreeTest.php
+++ tests/interfacesTreeTest.php
@@ -1,7 +1,24 @@
 <?php
 
+use src\interface_;
+
 class interfacesTreeTest extends PHPUnit\Framework\TestCase {
-    function test_1(){
+    function test_0(){
+        $interfaces = [];
+        
+        $tree = get_interfaces_tree( $interfaces );
+        
+        $this->assertEquals( 0, count( $tree ) );
         
     }
+//     function test_1(){
+//         $interfaces = [];
+        
+//         $interface = new interface_("parent");
+//         $interfaces[] = $interface;
+        
+        
+//     }
+// 
+
 }
