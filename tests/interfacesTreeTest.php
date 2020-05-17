c19e2a6d8e6b42fb7907da3b86f24d25aa847a39
rudymartinb <paraespam541@gmail.com>
2020-05-17 00:43:41 -0300

    autocommit por pruebas sin fallar

---
+++ tests/interfacesTreeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *  test case.
+ */
+class interfacesTreeTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated interfacesTreeTest::setUp()
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated interfacesTreeTest::tearDown()
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+}
+
