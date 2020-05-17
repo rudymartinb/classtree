33bf2a31a56aaf96e900f1078f0028be9f9978c0
rudymartinb <paraespam541@gmail.com>
2020-05-17 02:00:37 -0300

    autocommit por pruebas sin fallar

--- tests/microtests/filesTest.php
+++ tests/microtests/filesTest.php
@@ -8,18 +8,7 @@ use function files\get_classes_from_sources;
 use function files\get_interfaces_from_sources;
 use function files\get_interfaces;
 
-interface one {
-    
-}
-interface two extends one {
-    
-}
-interface three extends one {
-    
-}
-interface four extends two, three {
-    
-}
+
 
 class filesTest extends PHPUnit\Framework\TestCase {
     
