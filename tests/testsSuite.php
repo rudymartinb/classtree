4fad00f85ed6ab2736ff43b1576fa94f5f1d1072
rudymartinb <paraespam541@gmail.com>
2020-05-17 13:24:58 -0300

    autocommit: interfaces tree builder

--- tests/testsSuite.php
+++ tests/testsSuite.php
@@ -11,11 +11,8 @@ require_once 'src/get_interfaces_tree.php';
 
 require_once 'src/interface_.php';
 require_once 'src/class_.php';
-require_once 'src/ClassDiagram.php';
 
 require_once 'src/app.php';
-
-// require_once 'src/permutacion.php';
 
 /*
  * TESTS !!!
@@ -29,12 +26,9 @@ require_once 'tests/interfaceTest.php';
 require_once 'tests/treeTest.php';
 require_once 'tests/interfacesTreeTest.php';
 
-// TODO: eliminar?
-require_once 'tests/ClassDiagramTest.php';
 
 require_once 'tests/AppTest.php';
 
-// require_once 'tests/permutationTest.php';
 
 class testsSuite extends PHPUnit\Framework\TestSuite {
 
@@ -45,15 +39,12 @@ class testsSuite extends PHPUnit\Framework\TestSuite {
         
         $this->addTestSuite('claseTest');
         $this->addTestSuite('interfaceTest');
-        $this->addTestSuite('ClassDiagramTest');
         
         $this->addTestSuite('AppTest');
         
         $this->addTestSuite('treeTest');
         $this->addTestSuite('interfacesTreeTest');
         
-        
-//         $this->addTestSuite('combinatoriaTest');
     }
 
     public static function suite() {
