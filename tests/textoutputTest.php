f2b518d22b8594d800e7cfaf9d4fff042b17bc33
rudymartinb <paraespam541@gmail.com>
2020-05-17 19:02:18 -0300

    autocommit: added simple installation and runner scripts

---
+++ tests/textoutputTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *  test case.
+ */
+class textoutputTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated textoutputTest::setUp()
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated textoutputTest::tearDown()
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+}
+
