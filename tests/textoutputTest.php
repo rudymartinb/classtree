01089bacfcef228c7bdce6b471156878a8e61ea9
rudymartinb <paraespam541@gmail.com>
2020-05-17 19:35:28 -0300

    autocommit: added textoutput function

--- tests/textoutputTest.php
+++ tests/textoutputTest.php
@@ -45,7 +45,7 @@ class textoutputTest extends PHPUnit\Framework\TestCase {
         
         $tree = get_tree( $classes );
         $actual = textoutput( $tree );
-        echo "\n"; echo  $actual ;
+        echo "\n". $actual ;
         $this->assertEquals( "parent\n+ child\n" , $actual );
     }
     
