8687b1e6f6e10d8d11b09dfda620efb4ac827384
rudymartinb <paraespam541@gmail.com>
2020-05-17 22:17:44 -0300

    autocommit: fixing tree build and doing manual testing on filesystem

--- src/files.php
+++ src/files.php
@@ -100,7 +100,7 @@ function get_clases( string $source ) : Array {
 }
 
 function get_interfaces( string $source ) : Array {
-    $pattern  = "/[ ]*(?<tipo>interface )[ ]*";
+    $pattern  = "/[ ]*(?<tipo>interface(?: ))[ ]*";
     $pattern .= "(?<nombretipo>[0-9a-zA-Z_]+)[ ]*";
     $pattern .= "(extends (?<extends>[0-9a-zA-Z_,]*)|).*[ {]+";
     $pattern .= "/";
