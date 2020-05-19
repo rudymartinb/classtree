af8245769c2d16996a25de78fe2a3298f3cb0afc
rudymartinb <paraespam541@gmail.com>
2020-05-19 01:13:56 -0300

    autocommit: creating virtual grid

---
+++ tests/gridTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *  test case.
+ */
+class gridTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated gridTest::setUp()
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated gridTest::tearDown()
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+}
+
