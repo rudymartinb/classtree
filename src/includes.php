3dc6d33e8ccd049d38d5c9b351f448a419f295fa
rudymartinb <paraespam541@gmail.com>
2020-05-19 01:16:43 -0300

    autocommit: creating virtual grid

--- src/includes.php
+++ src/includes.php
@@ -3,6 +3,7 @@ function include_project_files( string $path ){
     require_once $path.'src/class_finder.php';
     require_once $path.'src/files.php';
     require_once $path.'src/get_tree.php';
+    require_once $path.'src/grid.php';
     
 //     require_once $path.'src/get_interfaces_tree.php';
 //     require_once $path.'src/interface_.php';
