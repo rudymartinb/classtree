da89caa8c4a81607752678e3280711cd488a1c6b
rudymartinb <paraespam541@gmail.com>
2020-05-19 01:24:34 -0300

    autocommit: creating virtual grid

--- src/grid.php
+++ src/grid.php
@@ -2,7 +2,24 @@
 namespace src;
 
 class grid {
+    
+    private $matrix = [];
+
     function add_element( class_ $class ){
-        
+        // by doing this we are adding a new "column" 
+        $this->matrix[] = [ $class ];
     }
+    function get_rows() : int{
+        $num_rows = 0;
+        foreach( $this->matrix as $column ){
+            if( $num_rows < count( $column ) ){
+                $num_rows =  count( $column ) ;
+            }
+        }
+        return count( $this->matrix );
+    }
+    function get_columns() : int{
+        return count( $this->matrix );
+    }
+    
 }
--- tests/gridTest.php
+++ tests/gridTest.php
@@ -7,10 +7,13 @@ class gridTest extends PHPUnit\Framework\TestCase {
     
     function test_1(){
         $class1 = new class_("class1");
-        $class2 = new class_("class2");
+//         $class2 = new class_("class2");
         
         $grid = new grid();
         $grid->add_element( $class1 );
+        
+        $this->assertEquals(1, $grid->get_rows());
+        $this->assertEquals(1, $grid->get_columns());
     }
 }
 
