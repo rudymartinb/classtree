9ecf9bdb37c582ffc98d4c4ed0cf8e4c5bac5925
rudymartinb <paraespam541@gmail.com>
2020-05-19 01:27:36 -0300

    autocommit: creating virtual grid

--- tests/gridTest.php
+++ tests/gridTest.php
@@ -7,7 +7,6 @@ class gridTest extends PHPUnit\Framework\TestCase {
     
     function test_1(){
         $class1 = new class_("class1");
-//         $class2 = new class_("class2");
         
         $grid = new grid();
         $grid->add_element( $class1 );
@@ -15,5 +14,19 @@ class gridTest extends PHPUnit\Framework\TestCase {
         $this->assertEquals(1, $grid->get_rows());
         $this->assertEquals(1, $grid->get_columns());
     }
+
+    function test_2(){
+        $class1 = new class_("class1");
+        $class2 = new class_("class2");
+        
+        $grid = new grid();
+        $grid->add_element( $class1 );
+        $grid->add_element( $class2 );
+        
+        $this->assertEquals(1, $grid->get_rows());
+        $this->assertEquals(2, $grid->get_columns());
+    }
+    
+    
 }
 
