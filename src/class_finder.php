41bcdeae479e087ba68a330162213624ea936e6f
rudymartinb <paraespam541@gmail.com>
2020-05-19 00:14:21 -0300

    autocommit: modfifying class_ to be used also with interfaces

--- src/class_finder.php
+++ src/class_finder.php
@@ -34,7 +34,7 @@ class class_finder {
         $lista = [];
         $namespace = "";
         foreach ($matches["tipo"] as $key => $value ) {
-            if( $matches[ "nsaceflag" ][ $key ] != "" ){
+            if( $matches[ "nsaceflag" ][ $key ] == "namespace" ){
                 $namespace = $matches[ "nsname" ][ $key ];
                 continue;
             }
