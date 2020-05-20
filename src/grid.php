577612768df6a886e6104a2a93f4d39799d94979
rudymartinb <paraespam541@gmail.com>
2020-05-20 00:44:59 -0300

    autocommit: creating virtual grid

--- src/grid.php
+++ src/grid.php
@@ -21,6 +21,43 @@ class grid {
         return $this->classes[$classname]["placed"];
     }
 
+    function draw(){
+        
+        //example
+        $img = imagecreatetruecolor(300, 200);
+        $white = imagecolorallocate($img, 255,   255,  255);
+        imagefilledrectangle($img, 0,0,299,299, $white);
+        
+        $gray   = imagecolorallocate($img, 224,   224,  224);
+        $this->draw_grid($img, 0,0,15,20,20,10,$gray);
+//         header("Content-type: image/png");
+        
+        
+        imagejpge($img,"/tmp/salida.jpg");
+        imagedestroy($img);
+        
+    }
+    function draw_grid(&$img, $x0, $y0, $width, $height, $cols, $rows, $color) {
+        //draw outer border
+        imagerectangle($img, $x0, $y0, $x0+$width*$cols, $y0+$height*$rows, $color);
+        //first draw horizontal
+        $x1 = $x0;
+        $x2 = $x0 + $cols*$width;
+        for ($n=0; $n<ceil($rows/2); $n++) {
+            $y1 = $y0 + 2*$n*$height;
+            $y2 = $y0 + (2*$n+1)*$height;
+            imagerectangle($img, $x1,$y1,$x2,$y2, $color);
+        }
+        //then draw vertical
+        $y1 = $y0;
+        $y2 = $y0 + $rows*$height;
+        for ($n=0; $n<ceil($cols/2); $n++) {
+            $x1 = $x0 + 2*$n*$width;
+            $x2 = $x0 + (2*$n+1)*$width;
+            imagerectangle($img, $x1,$y1,$x2,$y2, $color);
+        }
+    }
+    
     private $matrix = [];
     function distribute( string $parent = "" ){
         if( $parent == "" ){
@@ -36,8 +73,7 @@ class grid {
             $class = force_class( $class["class"] );
             $extends = $class->get_extends();
             
-            /* if theres any parent class
-             * we need to know if theres one we are looking for
+            /* 
              */
             if( $extends != [] ){
                 if( $parent == ""){
