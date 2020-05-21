f88e9cdbad6e416d22c7474c0148da325fff331d
rudymartinb <paraespam541@gmail.com>
2020-05-21 20:18:50 -0300

    autocommit: creating virtual grid

--- src/grid.php
+++ src/grid.php
@@ -160,7 +160,7 @@ class grid {
             imageline ( $this->img , $x1 , $y1 , $x2 , $y2 , $this->color["black"] );
             $points = array( $x1, $y1 , $xx1, $yy1 , $xx2, $yy2 );
             
-            imagepolygon($this->img, $points, 3, $this->color["while"]);
+            imagefilledpolygon($this->img, $points, 3, $this->color["white"]);
 //             imagepolygon($this->img, $points, 3, $this->color["black"]);
             
             
