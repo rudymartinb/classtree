50b792c1fef0f00d67745feefe49520ff525cbb0
rudymartinb <paraespam541@gmail.com>
2020-05-21 09:44:38 -0300

    autocommit: creating virtual grid

--- src/grid.php
+++ src/grid.php
@@ -56,8 +56,9 @@ class grid {
         $img = imagecreatetruecolor( $maxwidth, $maxheight);
         $white = imagecolorallocate($img, 255,   255,  255);
         $black = imagecolorallocate($img, 0,   0,  0);
-        imagerectangle($img, 0,0,299,299, $black);
+        
         imagefilledrectangle($img, 0,0,299,299, $white);
+        imagerectangle($img, 0,0,299,299, $black);
         
         $gray   = imagecolorallocate($img, 224,   224,  224);
 //         $this->draw_grid($img, 0,0,15,20,20,10,$gray);
