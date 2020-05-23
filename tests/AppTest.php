b10b5f131d23bdb23783aee96974002d5f26da98
rudymartinb <paraespam541@gmail.com>
2020-05-23 00:05:09 -0300

    autocommit: removing textoutput() and interfaces* class and tests

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -24,47 +24,21 @@ class AppTest extends PHPUnit\Framework\TestCase {
     }
     
     
-    function test_new(){
+//     function test_new(){
 
-        $app = new App();
-        $app->set_directory( "./tests/dummy" );
-        $file = "/tmp/output.txt";
-        $app->set_output_file($file);
-
-        $app->build();
-        
-        $this->assertFileExists( $file );
-        
-        return;
-        
-    }
-    
-//     function test_self(){
-        
 //         $app = new App();
-//         $app->set_directory( "./src" );
+//         $app->set_directory( "./tests/dummy" );
 //         $file = "/tmp/output.txt";
-        
 //         $app->set_output_file($file);
-        
-// //         $app->build();
-// echo "scanning" ;
-//         $app->scan_dir();
-//         echo "sources" ;
-//         $app->read_sources();
-//         echo "classes" ;
-//         $app->generate_classes();
-//         echo "trees" ;
-//         $app->create_trees();
-//         echo "file" ;
-//         $app->generate_file();
-        
+
+//         $app->build();
         
 //         $this->assertFileExists( $file );
         
 //         return;
         
 //     }
+
     
 }
 
