0f34e700a9ac37caa9424eecefdc4a008847fe94
rudymartinb <paraespam541@gmail.com>
2020-05-23 00:01:06 -0300

    autocommit: removing textoutput() and interfaces* class and tests

--- src/includes.php
+++ src/includes.php
@@ -4,9 +4,6 @@ function include_project_files( string $path ){
     require_once $path.'src/files.php';
     require_once $path.'src/get_tree.php';
     require_once $path.'src/grid.php';
-    
-//     require_once $path.'src/get_interfaces_tree.php';
-//     require_once $path.'src/interface_.php';
 
     require_once $path.'src/class_.php';
     
