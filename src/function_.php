a5d4f2240e1b0896120633dc1f915440d03eebcc
rudymartinb <paraespam541@gmail.com>
2020-05-23 20:45:49 -0300

    autocommit: adding *class methods* information to Tree()

--- src/function_.php
+++ src/function_.php
@@ -1,6 +1,48 @@
 <?php
 namespace src;
 
+
+class parameter_ {
+    private $type;
+    function get_type() : string {
+        return $this->type;
+    }
+    
+    private $name;
+    function get_name() : string {
+        return $this->name;
+    }
+    
+    function __construct( string $source ){
+        $this->type = $this->extract_mod( $source );
+        $this->name = $this->extract_name($source);
+    }
+    
+    private function extract_mod( string $source ){
+        $source = trim( $source );
+        $pos = strpos($source, "$");
+        
+        // dollar sign must be present
+        if( $pos === FALSE or $pos == 0){
+            return "";
+        }
+        return substr($source,0, $pos-1);
+        
+    }
+    
+    private function extract_name( string $source ){
+        $pos = strpos($source, "$");
+        
+        // dollar sign must be present
+        if( $pos === FALSE ){
+            return "";
+        }
+        return substr($source,$pos);
+    }
+    
+}
+
+
 // I call it "function" but it should be read as class method.
 // (keyword "function" is used by PHP, not my fault)
 class function_ {
--- tests/function_Test.php
+++ tests/function_Test.php
@@ -1,47 +1,8 @@
 <?php
 
 use src\function_;
+use src\parameter_;
 
-
-class parameter_ {
-    private $type;
-    function get_type() : string {
-        return $this->type; 
-    }
-    
-    private $name;
-    function get_name() : string {
-        return $this->name;
-    }
-    
-    function __construct( string $source ){
-        $this->type = $this->extract_mod( $source );
-        $this->name = $this->extract_name($source);
-    }
-    
-    private function extract_mod( string $source ){
-        $source = trim( $source );
-        $pos = strpos($source, "$");
-        
-        // dollar sign must be present
-        if( $pos === FALSE or $pos == 0){
-            return "";
-        }
-        return substr($source,0, $pos-1);
-        
-    }
-    
-    private function extract_name( string $source ){
-        $pos = strpos($source, "$");
-        
-        // dollar sign must be present
-        if( $pos === FALSE ){
-            return "";
-        }
-        return substr($source,$pos);
-    }
-    
-}
 
 class function_Test extends PHPUnit\Framework\TestCase {
     function test_get_mod1(){
