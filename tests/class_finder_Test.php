397faa3d67c6f2ed28dcf8ee1f01dce5fd57629f
rudymartinb <paraespam541@gmail.com>
2020-05-23 11:17:03 -0300

    autocommit: refactoring helper functions

---
+++ tests/class_finder_Test.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *  test case.
+ */
+class class_finder_Test extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated class_finder_Test::setUp()
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated class_finder_Test::tearDown()
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+}
+
