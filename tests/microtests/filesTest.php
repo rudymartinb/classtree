331e6a5e34599b1bbb33d1ee86bffa6a628050b7
rudymartinb <paraespam541@gmail.com>
2020-05-23 11:09:23 -0300

    autocommit: refactoring helper functions

--- tests/microtests/filesTest.php
+++ tests/microtests/filesTest.php
@@ -38,20 +38,7 @@ class filesTest extends PHPUnit\Framework\TestCase {
         $lista = get_all_files( $path );
         $this->assertEquals( 3, count( $lista ) );
     }
-    
 
-    /*
-     *     function test_get_files() {
-        $path = "/home/rudy/projects/classtree/tests/dummy";
-        $count = 0;
-        $function = function() use( &$count ) {
-            $count ++;
-        };
-        $lista = get_all_files( $path, $function );
-        $this->assertEquals( 3, count( $lista ) );
-        $this->assertEquals( 3, $count );
-    }
-     */
 
     function test_separate_php_fail(){
         $phpfiles = get_php_files( [] );
