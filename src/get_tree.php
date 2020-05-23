2c1f4a17eaa35d81fbc3d97bb7fd587e508a0aac
rudymartinb <paraespam541@gmail.com>
2020-05-22 21:21:30 -0300

    autocommit: converting get_tree() into a class

--- src/get_tree.php
+++ src/get_tree.php
@@ -92,7 +92,7 @@ class Tree {
             /* calculate total tree height for this element
              */
             $actual = 0;
-            $max = 0; // this parent element
+            $max = 0; // from this parent element
             foreach( $childrens as $child ){
                 $actual = $child["height"];
                 if( $actual > $max ){
