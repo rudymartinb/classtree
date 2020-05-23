f7bff31c82998b6b34383c68d141bf80af911698
rudymartinb <paraespam541@gmail.com>
2020-05-23 20:47:39 -0300

    autocommit: adding *class methods* information to Tree()

--- tests/function_Test.php
+++ tests/function_Test.php
@@ -36,23 +36,13 @@ class function_Test extends PHPUnit\Framework\TestCase {
     }
     
     
-//     function test_get_mod1(){
-//         $string1 = 'int $uno';
-//         $arr = explode( "$", $string1 );
-//         $mod = get_mod( $string1 );
-//         $var = get_var( $string1 );
-//         $this->assertEquals( "int ", $mod );
-//         $this->assertEquals( '$uno', $var );
-//     }
-    
-    
-//     function test_1(){
-//         $source = 'private function algo1( int $uno, string $dos ): string {';
-//         $fn = new function_( $source );
-//         $this->assertEquals( "algo1", $fn->get_name() );
-//         $this->assertEquals( 'int $uno', $fn->get_params()[0] );
+    function test_1(){
+        $source = 'private function algo1( int $uno, string $dos ): string {';
+        $fn = new function_( $source );
+        $this->assertEquals( "algo1", $fn->get_name() );
+        $this->assertEquals( '$uno', $fn->get_params()[0]->get_name() );
         
         
-//     }
+    }
 }
 
