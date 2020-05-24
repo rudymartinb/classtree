38672b2b2a2f00041702dd3d89a6305e047f5959
rudymartinb <paraespam541@gmail.com>
2020-05-23 23:36:58 -0300

    autocommit: adding *class methods* information to Tree()

--- tests/microtests/filesTest.php
+++ tests/microtests/filesTest.php
@@ -56,11 +56,11 @@ class filesTest extends PHPUnit\Framework\TestCase {
     function test_braces_4() {
         //         $filename = "./tests/dummy/prueba2.php";
         //         $source = get_source( $filename );
-        $source = "{ {} }{}";
+        $source = "{ {}{} }{}";
         
         $matches = mymatch($source);
                 var_dump( $matches[0] );
-        $this->assertEquals( "{ {} }", $matches[0][0] );
+        $this->assertEquals( "{ {}{} }", $matches[0][0] );
     }
     
     
