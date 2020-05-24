e18c93a1eda979949ee437edf16881200a5d2989
rudymartinb <paraespam541@gmail.com>
2020-05-23 21:09:52 -0300

    autocommit: adding *class methods* information to Tree()

--- tests/parameter_Test.php
+++ tests/parameter_Test.php
@@ -1,36 +1,7 @@
 <?php
 
-/**
- *  test case.
- */
-class parameter_Test extends PHPUnit_Framework_TestCase
-{
-
-    /**
-     * Prepares the environment before running a test.
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        // TODO Auto-generated parameter_Test::setUp()
-    }
-
-    /**
-     * Cleans up the environment after running a test.
-     */
-    protected function tearDown()
-    {
-        // TODO Auto-generated parameter_Test::tearDown()
-        parent::tearDown();
-    }
-
-    /**
-     * Constructs the test case.
-     */
-    public function __construct()
-    {
-        // TODO Auto-generated constructor
-    }
-}
+class parameter_Test extends PHPUnit\Framework\TestCase {
+    
+    
+|}
 
