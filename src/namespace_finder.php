34ab4d7f03d42c3f933346b3ca70d3f487d6793c
rudymartinb <paraespam541@gmail.com>
2020-05-25 18:23:23 -0300

    autocommit: splitting class_finder into namespace_finder

--- src/namespace_finder.php
+++ src/namespace_finder.php
@@ -51,13 +51,13 @@ class namespace_finder {
     	// line found
     	$line = $this->matches[0][$this->current_key];
     	
-    	$start_position = strpos( $source , $line )+strlen( $line )+1;
+    	$start_position = strpos( $source , $line )+strlen( $line );
     	$body_lenght = strlen( $source )-$start_position;
     	
     	$next_line = $this->matches[0][$this->current_key+1];
     	
     	if( $next_line != "" and $next_line !== null ){
-    		$body_lenght = $body_lenght - strpos( $source, $next_line )-1;
+    		$body_lenght = $body_lenght - strpos( $source, $next_line );
     	}
     	
     	return substr($this->source, $start_position, $body_lenght );
