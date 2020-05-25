b7f61f6a8410d9e81c05e870e889e1f1a395533e
rudymartinb <paraespam541@gmail.com>
2020-05-25 18:57:11 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

--- tests/namespace_finder_Test.php
+++ tests/namespace_finder_Test.php
@@ -55,7 +55,7 @@ function test() {
 }
 ';
 	$body = $finder->get_body();
-	var_dump( $body );
+	var_dump( $finder->matches($source) );
 	$this->assertEquals( $expected, $finder->get_body() );
 	$finder->next();
 	$this->assertEquals( false, $finder->more_elements() );
