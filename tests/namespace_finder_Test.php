aa6cd5c11dc2ff5bc194e8782e5b7f4b3cc9be99
rudymartinb <paraespam541@gmail.com>
2020-05-25 17:34:45 -0300

    autocommit: splitting class_finder into namespace_finder

--- tests/namespace_finder_Test.php
+++ tests/namespace_finder_Test.php
@@ -14,6 +14,7 @@ class namespace_finder_Test extends PHPUnit\Framework\TestCase {
 		$source = '';
 		
 		$finder = new namespace_finder( $source );
+
 		
 		$this->assertEquals( false, $finder->more_elements() );
 	}
@@ -31,7 +32,6 @@ class namespace_finder_Test extends PHPUnit\Framework\TestCase {
 		
 		$finder = new namespace_finder( $source );
 
-		var_dump($finder->matches($source));
 		$this->assertEquals( true, $finder->more_elements() );
 	}
 
