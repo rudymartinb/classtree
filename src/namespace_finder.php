02a20b813c97d1a173e3113d39486279dbe0c7a3
rudymartinb <paraespam541@gmail.com>
2020-05-25 21:16:00 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

--- src/namespace_finder.php
+++ src/namespace_finder.php
@@ -18,7 +18,7 @@ class namespace_finder {
         $this->pattern .= "(?<nsname>[0-9a-zA-Z_\\\\]+)";
         $this->pattern .= "(?:[^\n]*))";
         // rest of the namespace body is included
-        $this->pattern .= "(?:(?!(?R)).)*";
+        $this->pattern .= "(?<body>(?!(?R)).)*";
         $this->pattern .= "/ms";
         
         $this->matches($source);
