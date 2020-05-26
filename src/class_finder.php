2c126754f8c01398c4af36db10500ecb4a38c964
rudymartinb <paraespam541@gmail.com>
2020-05-26 00:47:53 -0300

    autocommit: rewritting class_finder to make a good use of preg_match

--- src/class_finder.php
+++ src/class_finder.php
@@ -17,7 +17,7 @@ class class_finder {
         $this->pattern .= "(?<classname>[0-9a-zA-Z_]+)";
         $this->pattern .= "(";
         $this->pattern .= "( extends (?<extends>[0-9a-zA-Z_]*))|";
-        $this->pattern .= "( implements (?<implements>((\s)*[0-9a-zA-Z_,]*)*))";
+        $this->pattern .= "( implements (?<implements>(\s*[0-9a-zA-Z_,]*)*))";
         $this->pattern .= ")*";
         $this->pattern .= "[ {]*";
         $this->pattern .= "))";
