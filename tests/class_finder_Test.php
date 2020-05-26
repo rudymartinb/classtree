dd7d915e350d44d0e8d94d3477950a78a1f37045
rudymartinb <paraespam541@gmail.com>
2020-05-26 00:11:09 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

--- tests/class_finder_Test.php
+++ tests/class_finder_Test.php
@@ -7,15 +7,17 @@ class class_finder_Test extends PHPUnit\Framework\TestCase {
 	function test_preg(){
 		$source =" mario 1 bros 2 ";
 		//		  12345678901234567
-		$pattern = "/[ ]*(?<mario>mario [0-9]*)|(?<bros> bros [0-9]*)";
-// 		$pattern = "/[ ]*(((?<mario>mario [0-9]*))|((<bros> bros [0-9]*)))*";
+		$pattern = "/(?:[ ]*)((?<mario>mario [0-9]*)|(?<bros> bros [0-9]*)){0,2}(?:[ ]*)";
 		$pattern .= "/ms";
 		
 		$matches = [];
 		preg_match_all($pattern, $source, $matches );
-		var_dump( $matches[0] );
+		var_dump( $matches );
 		$this->assertEquals( 2, count( $matches[0] ) );
-		
+
+		$source =" bros 2 mario 1 ";
+		var_dump( $matches );
+		$this->assertEquals( 2, count( $matches[0] ) );
 		
 	}
 	function test_zero(){
