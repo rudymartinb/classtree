cf94f2ba8d7eb960a049ec740671c8da81120311
rudymartinb <paraespam541@gmail.com>
2020-05-26 00:35:32 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

---
+++ tests/microtests/preg_match_all_Test.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+/**
+ *  test case.
+ */
+class preg_match_all_Test extends PHPUnit_Framework_TestCase
+{
+	
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		// TODO Auto-generated preg_match_all_Test::setUp()
+		
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		// TODO Auto-generated preg_match_all_Test::tearDown()
+		
+		parent::tearDown();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+		// TODO Auto-generated constructor
+	}
+
+	
+}
+
