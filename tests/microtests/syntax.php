7cc64f3d0509c7111e98f7a57c34b5710e18a7ca
rudymartinb <paraespam541@gmail.com>
2020-05-26 18:32:08 -0300

    autocommit: rewritting class_finder to make a good use of preg_match

--- tests/microtests/syntax.php
+++ tests/microtests/syntax.php
@@ -1,1 +1,17 @@
 <?php
+/* just write anything that might seems valid on it
+ * and remember to delete it once done.
+ */
+
+function
+test
+(
+		int $something 
+		)
+		:
+		int
+		{
+	return
+	0
+	;
+}
