6559f94263ed6c873b2ef0f5be0178ce6a8dce1e
rudymartinb <paraespam541@gmail.com>
2020-05-25 23:12:54 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

--- src/class_finder.php
+++ src/class_finder.php
@@ -14,9 +14,9 @@ class class_finder {
 //         $this->pattern .= "(?<abstract>abstract|)";
         $this->pattern .= "[ ]*(?: |)";
         $this->pattern .= "(?:class)(?: )[ ]*";
-        $this->pattern .= "(?<classname>[0-9a-zA-Z_]+)[ ]*";
+        $this->pattern .= "(?<classname>[0-9a-zA-Z_]+)";
 //         $this->pattern .= "( implements (?<implements>[0-9a-zA-Z_, ]*)|)[ ]*";
-        $this->pattern .= "(extends (?<extends>[0-9a-zA-Z_]*)|).*[ {]*";
+        $this->pattern .= "( extends (?<extends>[0-9a-zA-Z_]*)|).*[ {]*";
         $this->pattern .= "))";
         $this->pattern .= "/ms";
         
