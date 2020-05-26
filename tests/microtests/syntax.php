5ab4666f6ec0893f00145c41b80673ef56f4318e
rudymartinb <paraespam541@gmail.com>
2020-05-26 18:34:00 -0300

    autocommit: rewritting class_finder to make a good use of preg_match

--- tests/microtests/syntax.php
+++ tests/microtests/syntax.php
@@ -1,6 +1,12 @@
 <?php
 /* just write anything that might seems valid on it
  * and remember to delete it once done.
+ * 
+ * saving the file while running the runtests.sh on script directory
+ * will tell you inmediatly if it's valid or not.
+ * 
+ * since it loads from the test suite 
+ * it won't affect the production code.
  */
 
 function
