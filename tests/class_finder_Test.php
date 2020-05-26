2cc7b60c9e546c5d071a90febbeb386c5a18b8b2
rudymartinb <paraespam541@gmail.com>
2020-05-25 23:15:20 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

--- tests/class_finder_Test.php
+++ tests/class_finder_Test.php
@@ -33,11 +33,18 @@ class class_finder_Test extends PHPUnit\Framework\TestCase {
 		$source = 'class test implements something';
 		$finder = new class_finder( $source );
 		
-		var_dump( $finder->matches($source));
-		
 		$this->assertEquals( true, $finder->more_elements() );
 		$this->assertEquals( "test", $finder->get_name() );
 		$this->assertEquals( "something", $finder->get_implements() );
+	}
+
+	function test_class_implements_2(){
+		$source = 'class test implements something,else';
+		$finder = new class_finder( $source );
+		
+		$this->assertEquals( true, $finder->more_elements() );
+		$this->assertEquals( "test", $finder->get_name() );
+		$this->assertEquals( "something,else", $finder->get_implements() );
 	}
 	
 	
