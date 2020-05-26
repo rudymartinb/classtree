2a40fe40eca2235ce23a8ee81c0ed1eac571ba6f
rudymartinb <paraespam541@gmail.com>
2020-05-26 00:35:04 -0300

    autocommit: namespace_finder: new methods as replacement of preg array iteration

--- tests/class_finder_Test.php
+++ tests/class_finder_Test.php
@@ -4,43 +4,7 @@ use src\class_finder;
 use function files\get_source;
 
 class class_finder_Test extends PHPUnit\Framework\TestCase {
-	function test_preg(){
-		$source =" mario 1 2 3 bros 2 3 1 ";
-		//		  12345678901234567
-		$pattern = "/(?:[ ]*)";
-		$pattern .= "(";
-		$pattern .= "(?:[ ]*)";
-		$pattern .= "(?<mario>mario([\s\,]*[0-9])*)";
-		$pattern .= "(?:[ ]*)|";
-		$pattern .= "(?:[ ]*)";
-		$pattern .= "(?<bros>bros([\s\,]*[0-9])*)";
-		$pattern .= "(?:[ ]*)";
-		$pattern .= ")*";
-		
-		$pattern .= "/ms";
-		
-		$matches = [];
-		preg_match_all($pattern, $source, $matches );
-		$this->assertEquals( "mario 1 2 3", $matches["mario"][0] );
-		$this->assertEquals( "bros 2 3 1", $matches["bros"][0] );
-
-		$source =" bros 2 3 1 mario 1 2 3";
-		preg_match_all($pattern, $source, $matches );
-		$this->assertEquals( "mario 1 2 3", $matches["mario"][0] );
-		$this->assertEquals( "bros 2 3 1", $matches["bros"][0] );
-
-		$source =" bros 2,3,1 mario 1 2 3";
-		preg_match_all($pattern, $source, $matches );
-		$this->assertEquals( "mario 1 2 3", $matches["mario"][0] );
-		$this->assertEquals( "bros 2,3,1", $matches["bros"][0] );
-
-		$source =" bros 2, 3, 1 mario 1 2 3";
-		preg_match_all($pattern, $source, $matches );
-		$this->assertEquals( "mario 1 2 3", $matches["mario"][0] );
-		$this->assertEquals( "bros 2, 3, 1", $matches["bros"][0] );
-		
-		
-	}
+	
 	function test_zero(){
 		$source = '';
 		
