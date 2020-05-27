3898639983fd1d24e1ac5e247a427a82b70e0f54
rudymartinb <paraespam541@gmail.com>
2020-05-27 17:44:10 -0300

    autocommit: adding trait_finder

--- tests/microtests/preg/discard_Test.php
+++ tests/microtests/preg/discard_Test.php
@@ -1,32 +1,5 @@
 <?php
 
-/**
- *  test case.
- */
-class discard_Test extends PHPUnit_Framework_TestCase {
-
-	/**
-	 * Prepares the environment before running a test.
-	 */
-	protected function setUp() {
-		parent::setUp ();
-
-		// TODO Auto-generated discard_Test::setUp()
-	}
-
-	/**
-	 * Cleans up the environment after running a test.
-	 */
-	protected function tearDown() {
-		// TODO Auto-generated discard_Test::tearDown()
-		parent::tearDown ();
-	}
-
-	/**
-	 * Constructs the test case.
-	 */
-	public function __construct() {
-		// TODO Auto-generated constructor
-	}
+class discard_Test extends PHPUnit\Framework\TestCase {
+	
 }
-
--- tests/testsSuite.php
+++ tests/testsSuite.php
@@ -6,6 +6,7 @@ include_project_files("./");
  * TESTS !!!
  */
 require_once 'tests/microtests/preg_match_all_Test.php';
+require_once 'tests/microtests/preg/discard_Test.php';
 require_once 'tests/microtests/filesTest.php';
 
 require_once 'tests/parameter_Test.php';
@@ -36,6 +37,7 @@ class testsSuite extends PHPUnit\Framework\TestSuite {
     public function __construct() {
         $this->setName('testsSuite');
         $this->addTestSuite('preg_match_all_Test');
+        $this->addTestSuite('discard_Test');
         
         $this->addTestSuite('namespace_finder_Test');
         $this->addTestSuite('class_finder_Test');
