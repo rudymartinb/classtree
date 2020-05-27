6a9468ff362c67e1fa193126637562388861bf89
rudymartinb <paraespam541@gmail.com>
2020-05-26 22:47:01 -0300

    autocommit: adding trait_finder

--- tests/trait_finder_Test.php
+++ tests/trait_finder_Test.php
@@ -1,43 +1,32 @@
 <?php
 
-
-
 /**
  *  test case.
  */
-class trait_finder_Test extends PHPUnit_Framework_TestCase
-{
-	
+class trait_finder_Test extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * Prepares the environment before running a test.
 	 */
-	protected function setUp()
-	{
-		parent::setUp();
+	protected function setUp() {
+		parent::setUp ();
 
 		// TODO Auto-generated trait_finder_Test::setUp()
-		
 	}
 
 	/**
 	 * Cleans up the environment after running a test.
 	 */
-	protected function tearDown()
-	{
+	protected function tearDown() {
 		// TODO Auto-generated trait_finder_Test::tearDown()
-		
-		parent::tearDown();
+		parent::tearDown ();
 	}
 
 	/**
 	 * Constructs the test case.
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 		// TODO Auto-generated constructor
 	}
-
-	
 }
 
