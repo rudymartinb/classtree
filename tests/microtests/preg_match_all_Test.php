fd01d7474c9de47b6f37cbb89a87fe496a151061
rudymartinb <paraespam541@gmail.com>
2020-05-27 17:41:05 -0300

    autocommit: adding trait_finder

--- tests/microtests/preg_match_all_Test.php
+++ tests/microtests/preg_match_all_Test.php
@@ -35,62 +35,38 @@ class preg_match_all_Test extends PHPUnit\Framework\TestCase {
 	
 	function test_preg_just_3_braces(){
 		$source = "{}}";
-		
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 	
 	function test_preg_just_2_braces_with_something(){
 		$source = "{a}}{}";
-
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{a}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 
 	function test_preg_just_2_braces_with_something_2(){
 		$source = "a{a}a{a}a";
-		
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{a}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 	
 	function test_preg_more_braces(){
 		$source = "{a{a}}";
-
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{a{a}}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 	
 	function test_preg_a_lote_more_braces(){
 		$source = "{{{{{{{a}}}}}}}";
-
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{{{{{{{a}}}}}}}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 
 	function test_with_junk(){
 		$source = " asdf {a{a{a{a{a{{a}}a}a}a}a}} asdf";
-
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{a{a{a{a{a{{a}}a}a}a}a}}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 
 
