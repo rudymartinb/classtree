076352afdaefa404a910cea07b3a8d679bff00b8
rudymartinb <paraespam541@gmail.com>
2020-05-27 17:40:15 -0300

    autocommit: adding trait_finder

--- tests/microtests/preg_match_all_Test.php
+++ tests/microtests/preg_match_all_Test.php
@@ -19,15 +19,18 @@ class preg_match_all_Test extends PHPUnit\Framework\TestCase {
 	// ok  private $pattern = "/({(.*)})|({(?R)})/";
 	
 	# "/[^{}]*\{(?R)\}/mx";
-	
+	function run_grep_test( string $source, string $expected ){
+		$matches = [];
+		preg_match_all($this->pattern, $source, $matches );
+		
+		$actual = $matches[0][0];
+		$this->assertEquals($expected, $actual);
+	}
 	function test_preg_just_2_braces(){
 		$source = "{}";
 		
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
 		$expected = '{}';
-		$actual = $matches[0][0];
-		$this->assertEquals($expected, $actual);
+		$this->run_grep_test($source, $expected);
 	}
 	
 	function test_preg_just_3_braces(){
