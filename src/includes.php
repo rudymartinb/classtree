49f211da1b321ed3be9727eeb24ecd1ab98072c8
rudymartinb <paraespam541@gmail.com>
2020-05-26 22:49:29 -0300

    autocommit: adding trait_finder

--- src/includes.php
+++ src/includes.php
@@ -4,14 +4,16 @@ function include_project_files( string $path ){
     require_once $path.'src/function_.php';
     require_once $path.'src/class_.php';
     require_once $path.'src/class_finder.php';
+    require_once $path.'src/trait_finder.php';
     require_once $path.'src/namespace_finder.php';
+    
     require_once $path.'src/files.php';
-    require_once $path.'src/Trees.php';
-    require_once $path.'src/grid.php';
+//     require_once $path.'src/Trees.php';
+//     require_once $path.'src/grid.php';
 
     
     
-    require_once $path.'src/app.php';
+//     require_once $path.'src/app.php';
     
 }
 ?>
