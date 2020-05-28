c8fd8ff815bd9c3d21d1be7d8f5ca5e44fdc628c
rudymartinb <paraespam541@gmail.com>
2020-05-27 21:03:56 -0300

    autocommit: adding trait_finder

--- tests/microtests/preg/discard_Test.php
+++ tests/microtests/preg/discard_Test.php
@@ -1,7 +1,29 @@
 <?php
 
 class discard_Test extends PHPUnit\Framework\TestCase {
-	private $pattern = "/discard me/x";
+	private $pattern = '/".*"(?R)*/x';
+
+	function test_do_not(){
+		$source = '"do not"';
+		
+		$expected = '"do not"';
+		$this->run_grep_test($source, $expected);
+	}
+	
+	function test_basic(){
+		$source = "discard \"me\" after this";
+		
+		$expected = '"me"';
+		$this->run_grep_test($source, $expected);
+	}
+
+// 	function test_do_not_discard_me(){
+// 		$source = "do not discard me!";
+		
+// 		$expected = 'do not !';
+// 		$this->run_grep_test($source, $expected);
+// 	}
+
 	
 	function run_grep_test( string $source, string $expected ){
 		$matches = [];
@@ -9,13 +31,6 @@ class discard_Test extends PHPUnit\Framework\TestCase {
 		
 		$actual = $matches[0][0];
 		$this->assertEquals($expected, $actual);
-	}
-	
-	function test_preg_just_2_braces(){
-		$source = "discard me";
-		
-		$expected = '';
-		$this->run_grep_test($source, $expected);
 	}
 	
 }
