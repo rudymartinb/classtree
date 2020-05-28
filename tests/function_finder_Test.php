cf683e6503908886b58425f00ae0f17f6b24d45c
rudymartinb <paraespam541@gmail.com>
2020-05-28 20:27:41 -0300

    autocommit: adding function_finder()

--- tests/function_finder_Test.php
+++ tests/function_finder_Test.php
@@ -1,32 +1,12 @@
 <?php
 
-/**
- *  test case.
+/* same as traits
+ * based on the body of a class
+ * look for functions tags
+ * and then split arguments and return value
+ * 
  */
-class function_finder_Test extends PHPUnit_Framework_TestCase {
-
-	/**
-	 * Prepares the environment before running a test.
-	 */
-	protected function setUp() {
-		parent::setUp ();
-
-		// TODO Auto-generated function_finder_Test::setUp()
-	}
-
-	/**
-	 * Cleans up the environment after running a test.
-	 */
-	protected function tearDown() {
-		// TODO Auto-generated function_finder_Test::tearDown()
-		parent::tearDown ();
-	}
-
-	/**
-	 * Constructs the test case.
-	 */
-	public function __construct() {
-		// TODO Auto-generated constructor
-	}
+class function_finder_Test extends PHPUnit\Framework\TestCase {
+	
+	
 }
-
--- tests/testsSuite.php
+++ tests/testsSuite.php
@@ -2,31 +2,35 @@
 require_once 'src/includes.php';
 include_project_files("./");
 
-/*
- * TESTS !!!
+/* microtests 
  */
 require_once 'tests/microtests/preg_match_all_Test.php';
 require_once 'tests/microtests/preg/discard_Test.php';
 require_once 'tests/microtests/filesTest.php';
 
-require_once 'tests/parameter_Test.php';
+// todo: review to see if they are still needed
 require_once 'tests/function_Test.php';
 require_once 'tests/class_Test.php';
+require_once 'tests/parameter_Test.php';
+require_once 'tests/gridTest.php';
+require_once 'tests/AppTest.php';
+
+
+/* test cases
+ */
+require_once 'tests/function_finder_Test.php';
 require_once 'tests/class_finder_Test.php';
 require_once 'tests/namespace_finder_Test.php';
 require_once 'tests/trait_finder_Test.php';
 
 require_once 'tests/tree2_Test.php';
 
-require_once 'tests/gridTest.php';
-
-require_once 'tests/AppTest.php';
 
 
 /*
  * the following file is intended to test 
  * how things work in php
- * just write anything that might seems valid on it
+ * just write anything on the file that might seems valid on it
  * and remember to delete it once done.
  */
 require_once 'tests/microtests/syntax.php';
@@ -48,7 +52,7 @@ class testsSuite extends PHPUnit\Framework\TestSuite {
         $this->addTestSuite('tree2_Test');
         
         $this->addTestSuite('filesTest');
-//         $this->addTestSuite('function_Test');
+		$this->addTestSuite('function_finder_Test');
 //         $this->addTestSuite('class_Test');
 
 
