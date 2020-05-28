b5446d4309775fc9781b5b305dccfe55023039b9
rudymartinb <paraespam541@gmail.com>
2020-05-28 17:07:58 -0300

    autocommit: adding function_finder()

---
+++ tests/function_finder_Test.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+/**
+ *  test case.
+ */
+class function_finder_Test extends PHPUnit_Framework_TestCase
+{
+	
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+		// TODO Auto-generated function_finder_Test::setUp()
+		
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		// TODO Auto-generated function_finder_Test::tearDown()
+		
+		parent::tearDown();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+		// TODO Auto-generated constructor
+	}
+
+	
+}
+
