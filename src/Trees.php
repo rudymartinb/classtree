bef4d27f6eae5fe8e72f81053c632e6b368e0427
rudymartinb <paraespam541@gmail.com>
2020-05-28 09:11:19 -0300

    autocommit: adding trait_finder

--- src/Trees.php
+++ src/Trees.php
@@ -11,8 +11,12 @@ class tree2 {
 	
 	private $sources = [];
 	private $current_key = 0;
-	function more_elements(){
+	function more_elements() : bool {
 		return count( $this->sources ) > $this->current_key;
+	}
+	
+	function add_source( string $source ){
+		$this->sources[] = $source;
 	}
 }
 /* from the array of classes
--- tests/tree2_Test.php
+++ tests/tree2_Test.php
@@ -4,11 +4,22 @@ use src\tree2;
 
 class tree2_Test extends PHPUnit\Framework\TestCase {
 
-	function test_1(){
-
-		$source = "";
+	function test_empty(){
 		$tree = new tree2();
 		
 		$this->assertEquals( false, $tree->more_elements());
 	}
+	
+	function test_add_source(){
+		
+		$source = "";
+		$tree = new tree2();
+		
+		$tree->add_source( $source );
+		
+		$this->assertEquals( true, $tree->more_elements());
+	}
+	
+	
+	
 }
