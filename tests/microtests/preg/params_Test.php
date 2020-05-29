fcace97b2b1d5597f929d459dcd1073bf59ca1f5
rudymartinb <paraespam541@gmail.com>
2020-05-29 00:42:01 -0300

    autocommit: adding function_finder()

---
+++ tests/microtests/preg/params_Test.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *  test case.
+ */
+class params_Test extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp ();
+
+		// TODO Auto-generated params_Test::setUp()
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		// TODO Auto-generated params_Test::tearDown()
+		parent::tearDown ();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct() {
+		// TODO Auto-generated constructor
+	}
+}
+
