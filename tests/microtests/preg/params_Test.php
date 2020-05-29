03d918a434ff0cd0575dc731e686b627f63fee3b
rudymartinb <paraespam541@gmail.com>
2020-05-29 01:05:41 -0300

    autocommit: adding function_finder()

--- tests/microtests/preg/params_Test.php
+++ tests/microtests/preg/params_Test.php
@@ -4,9 +4,9 @@ class params_Test extends PHPUnit\Framework\TestCase {
 	
 	private $pattern;
 	function setUp() : void {
-		$this->pattern  = "/^";
+		$this->pattern  = "/";
 // 		$this->pattern .= "((?<partype>[a-zA-Z0-9_]*) )\s*";
-		$this->pattern .= "(\$(?<parname>[a-zA-Z0-9_]*))\s*";
+		$this->pattern .= "((?<parname>[a-zA-Z0-9_]*)\s*)\s*";
 // 		$this->pattern .= ",.)*";
 		$this->pattern .= "/mxs";
 		
@@ -15,23 +15,20 @@ class params_Test extends PHPUnit\Framework\TestCase {
 	function test_nothing(){
 		$source = '';
 		
-		
 		$matches = [];
 		preg_match_all($this->pattern, $source, $matches );
-// 		var_dump($matches[0]);
-		
 		$this->assertTrue( count( $matches ) > 0);
 	}
 
 	
 	function test_just_name(){
-		$source = '$sarasa';
+		$source = 'sarasa';
 		
 		$matches = [];
 		preg_match_all($this->pattern, $source, $matches );
 		var_dump($matches[0]);
 		
-		$this->assertTrue( count( $matches ) > 0);
+		$this->assertEquals( "sarasa", $matches["parname"][0] );
 	}
 	
 	
