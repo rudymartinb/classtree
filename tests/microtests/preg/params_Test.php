100e2cbde521d7acd1bbbe6de74f50f6c63a8af7
rudymartinb <paraespam541@gmail.com>
2020-05-30 14:07:05 -0300

    autocommit: adding function_finder()

--- tests/microtests/preg/params_Test.php
+++ tests/microtests/preg/params_Test.php
@@ -4,12 +4,12 @@ class params_Test extends PHPUnit\Framework\TestCase {
 	
 	private $pattern;
 	function setUp() : void {
-		$this->pattern  = "/";
-		$this->pattern .= "((?<partype>[a-zA-Z0-9_]*) )\s*";
-		$this->pattern .= '(\\$*(?<parname>[a-zA-Z0-9_]*)\s*)\s*';
-// 		$this->pattern .= ",.)*";
-		$this->pattern .= "/mxs";
+// 		$this->pattern  = '/(((?<partype>[a-zA-Z0-9_]*) )\s*(\$*(?<parname>[a-zA-Z0-9_]*)\s*)\s*,\s*)*/ms';
 		
+// 		$this->pattern  = '/(((?:(?<partype>[a-zA-Z0-9_]*)\s{1})*(\$*(?<parname>[a-zA-Z0-9_]*)\s*)\s*[,]{0,1}\s*)*)*/mxs';
+		
+		$this->pattern  = '/\$(?<parname>[a-zA-Z0-9_]*)/mxs';
+// 		$this->pattern  = '/(?<parname>[a-zA-Z0-9_]*)*/mxs';
 	}
 	
 	function test_nothing(){
@@ -26,22 +26,38 @@ class params_Test extends PHPUnit\Framework\TestCase {
 		
 		$matches = [];
 		preg_match_all($this->pattern, $source, $matches );
-		var_dump($matches[0]);
+		var_dump($matches[0][0]);
+		var_dump($matches["parname"]);
 		
 		$this->assertEquals( "sarasa", $matches["parname"][0] );
 	}
 
 	
-	function test_type_and_name(){
-		$source = 'int $sarasa';
+// 	function test_type_and_name(){
+// 		$source = 'int $sarasa';
 		
-		$matches = [];
-		preg_match_all($this->pattern, $source, $matches );
-		var_dump($matches[0]);
+// 		$matches = [];
+// 		preg_match_all($this->pattern, $source, $matches );
+// // 		var_dump($matches[0]);
 		
-		$this->assertEquals( "int", $matches["partype"][0] );
-		$this->assertEquals( "sarasa", $matches["parname"][0] );
-	}
+// 		$this->assertEquals( "int", $matches["partype"][0] );
+// 		$this->assertEquals( "sarasa", $matches["parname"][0] );
+// 	}
+
+// 	function test_2_params(){
+// 		$source = 'int $sarasa, string $acanomas';
+		
+// 		$matches = [];
+// 		preg_match_all($this->pattern, $source, $matches );
+// 		var_dump($matches["partype"]);
+// 		var_dump($matches["parname"]);
+		
+// 		$this->assertEquals( "int", $matches["partype"][0] );
+// 		$this->assertEquals( "sarasa", $matches["parname"][0] );
+// 		$this->assertEquals( "string", $matches["partype"][1] );
+// 		$this->assertEquals( "acanomas", $matches["parname"][1] );
+		
+// 	}
 	
 	
 }
