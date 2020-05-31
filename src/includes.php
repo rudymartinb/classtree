bf08da26cd9c7ab07158cbbc92330107caff265f
rudymartinb <paraespam541@gmail.com>
2020-05-30 23:14:48 -0300

    autocommit: refactoring sources

--- src/includes.php
+++ src/includes.php
@@ -2,12 +2,9 @@
 function include_project_files( string $path ){
 	require_once $path.'src/traits/finder.php';
 	
-//     require_once $path.'src/parameter_.php';
 
 	require_once $path.'src/parameters_finder.php';
-//     require_once $path.'src/function_.php';
     require_once $path.'src/function_finder.php';
-//     require_once $path.'src/class_.php';
     require_once $path.'src/class_finder.php';
     require_once $path.'src/trait_finder.php';
     require_once $path.'src/namespace_finder.php';
@@ -16,12 +13,6 @@ function include_project_files( string $path ){
     
     // tree2
     require_once $path.'src/Trees.php';
-    
-//     require_once $path.'src/grid.php';
 
-    
-    
-//     require_once $path.'src/app.php';
-    
 }
 ?>
