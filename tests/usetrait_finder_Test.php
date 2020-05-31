0709fb8d62dd842a784b941d2021992c64b3fbd9
rudymartinb <paraespam541@gmail.com>
2020-05-31 11:29:11 -0300

    autocommit: adding usetrait_finder()

--- tests/usetrait_finder_Test.php
+++ tests/usetrait_finder_Test.php
@@ -7,7 +7,12 @@ class usetrait_finder_Test extends PHPUnit\Framework\TestCase {
 	function test_empty(){
 		$finder = new usetrait_finder("");
 		$this->assertEquals( false, $finder->more_elements() );
-		
 	}
+
+	function test_somethingelse(){
+		$finder = new usetrait_finder("asdfasdf");
+		$this->assertEquals( false, $finder->more_elements() );
+	}
+	
 }
 
