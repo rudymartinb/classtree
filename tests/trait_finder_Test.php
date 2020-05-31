dfdd2abdba1636557c4db167db2009266cd3725f
rudymartinb <paraespam541@gmail.com>
2020-05-31 01:14:25 -0300

    autocommit: adding interface_finder()

--- tests/trait_finder_Test.php
+++ tests/trait_finder_Test.php
@@ -83,6 +83,31 @@ interface mytest_class {
 		$this->run_test_2bodies($source);
 	}
 	
+	function test_functions(){
+		$body = '{
+function something1();
+function something2( int $ant, string $strong );
+}';
+		$source = 'trait myif '.$body;
+		
+		$finder = new trait_finder( $source );
+		
+		// function something1
+		$this->assertEquals( true, $finder->more_elements() );
+		$this->assertEquals( "myif", $finder->get_name() );
+		$this->assertEquals( true, $finder->has_functions() );
+		$this->assertEquals( "something1", $finder->get_function_name() );
+		$this->assertEquals( false, $finder->has_parameters() );
+		$finder->next_function();
+		$this->assertEquals( "something2", $finder->get_function_name() );
+		$finder->next_parameter();
+		$this->assertEquals( true, $finder->has_parameters() );
+		$this->assertEquals( "int", $finder->get_parameter_type() );
+		$this->assertEquals( "ant", $finder->get_parameter_name() );
+		
+	}
+	
+	
 	function run_test_2bodies( string $source ){
 		$finder = new trait_finder($source);
 		
