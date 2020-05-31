4061f4489d1a9611f456f03d34c75763ba3fe229
rudymartinb <paraespam541@gmail.com>
2020-05-31 18:33:32 -0300

    autocommit: adding diagram element()

--- src/diagram/element.php
+++ src/diagram/element.php
@@ -4,4 +4,21 @@ class element {
 	function is_placed() : bool{
 		return false;
 	}
+	
+	private $type = "";
+	function set_type( string $type ){
+		$this->type = $type;
+	}
+	function get_type() : string {
+		return $this->type;
+	}
+	
+	private $name = "";
+	function set_name( string $name ){
+		$this->name = $name;
+	}
+	function get_name() : string {
+		return $this->name;
+	}
+	
 }
--- tests/diagram/element_Test.php
+++ tests/diagram/element_Test.php
@@ -9,4 +9,16 @@ class element_Test extends PHPUnit\Framework\TestCase {
 		$this->assertFalse( $element->is_placed() );
 	}
 
+	function test_attributes(){
+		$element = new element();
+		
+		$element->set_type("class");
+		$element->set_name("class1");
+		
+		$this->assertEquals( "class", $element->get_type() );
+		$this->assertEquals( "class1", $element->get_name() );
+		
+		$this->assertFalse( $element->is_placed() );
+	}
+	
 }
