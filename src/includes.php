22dffdb7b55447dff75ab5a42b2e036251651726
rudymartinb <paraespam541@gmail.com>
2020-05-31 18:17:01 -0300

    autocommit: adding traits to class_finder()

--- src/includes.php
+++ src/includes.php
@@ -1,21 +1,22 @@
 <?php
 function include_project_files( string $path ){
-	require_once $path.'src/traits/finder.php';
-	require_once $path.'src/traits/finder_functions.php';
+	// traits
+	require_once $path.'src/src/traits/finder.php';
+	require_once $path.'src/src/traits/finder_functions.php';
 	
-
-	require_once $path.'src/parameters_finder.php';
-    require_once $path.'src/function_finder.php';
-    require_once $path.'src/class_finder.php';
-    require_once $path.'src/interface_finder.php';
-    require_once $path.'src/trait_finder.php';
-    require_once $path.'src/usetrait_finder.php';
-    require_once $path.'src/namespace_finder.php';
+	// source scanners
+	require_once $path.'src/src/parameters_finder.php';
+    require_once $path.'src/src/function_finder.php';
+    require_once $path.'src/src/class_finder.php';
+    require_once $path.'src/src/interface_finder.php';
+    require_once $path.'src/src/trait_finder.php';
+    require_once $path.'src/src/usetrait_finder.php';
+    require_once $path.'src/src/namespace_finder.php';
     
     require_once $path.'src/files.php';
     
     // tree2
-    require_once $path.'src/Trees.php';
+//     require_once $path.'src/Trees.php';
 
 }
 ?>
--- tests/testsSuite.php
+++ tests/testsSuite.php
@@ -55,7 +55,7 @@ class testsSuite extends PHPUnit\Framework\TestSuite {
         $this->addTestSuite('usetrait_finder_Test');
         $this->addTestSuite('function_finder_Test');
         
-        $this->addTestSuite('tree2_Test');
+//         $this->addTestSuite('element_Test');
         
     }
 
