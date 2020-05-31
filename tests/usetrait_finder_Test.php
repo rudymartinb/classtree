ff2abd4de48fa42e89911d439a8ee17310285664
rudymartinb <paraespam541@gmail.com>
2020-05-31 14:39:42 -0300

    autocommit: adding usetrait_finder()

--- tests/usetrait_finder_Test.php
+++ tests/usetrait_finder_Test.php
@@ -4,21 +4,21 @@
 use src\usetrait_finder;
 
 class usetrait_finder_Test extends PHPUnit\Framework\TestCase {
-// 	function test_empty(){
-// 		$finder = new usetrait_finder("");
-// 		$this->assertEquals( false, $finder->more_elements() );
-// 	}
+	function test_empty(){
+		$finder = new usetrait_finder("");
+		$this->assertEquals( false, $finder->more_elements() );
+	}
 
-// 	function test_somethingelse(){
-// 		$finder = new usetrait_finder("asdfasdf");
-// 		$this->assertEquals( false, $finder->more_elements() );
-// 	}
+	function test_somethingelse(){
+		$finder = new usetrait_finder("asdfasdf");
+		$this->assertEquals( false, $finder->more_elements() );
+	}
 
-// 	function test_basic(){
-// 		$finder = new usetrait_finder("use sometrait;");
-// 		$this->assertEquals( true, $finder->more_elements() );
-// 		$this->assertEquals( "sometrait", $finder->get_trait_name() );
-// 	}
+	function test_basic(){
+		$finder = new usetrait_finder("use sometrait;");
+		$this->assertEquals( true, $finder->more_elements() );
+		$this->assertEquals( "sometrait", $finder->get_trait_name() );
+	}
 
 	function test_basic_2(){
 		$source = "use case2;
