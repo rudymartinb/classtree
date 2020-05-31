24004220379cac222099e2ceff06308fc75622d0
rudymartinb <paraespam541@gmail.com>
2020-05-30 23:35:51 -0300

    autocommit: adding interface_finder()

---
+++ tests/interface_finder_Test.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *  test case.
+ */
+class interface_finder_Test extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		parent::setUp ();
+
+		// TODO Auto-generated interface_finder_Test::setUp()
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		// TODO Auto-generated interface_finder_Test::tearDown()
+		parent::tearDown ();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct() {
+		// TODO Auto-generated constructor
+	}
+}
+
