564bd231ca72e6019ed512b89bd443dc8e21a11c
rudymartinb <paraespam541@gmail.com>
2020-05-31 21:50:36 -0300

    autocommit: adding diagram element()

--- tests/microtests/string_size_in_pixels_Test.php
+++ tests/microtests/string_size_in_pixels_Test.php
@@ -1,43 +1,32 @@
 <?php
 
-
-
 /**
  *  test case.
  */
-class string_size_in_pixels_Test extends PHPUnit_Framework_TestCase
-{
-	
+class string_size_in_pixels_Test extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * Prepares the environment before running a test.
 	 */
-	protected function setUp()
-	{
-		parent::setUp();
+	protected function setUp() {
+		parent::setUp ();
 
 		// TODO Auto-generated string_size_in_pixels_Test::setUp()
-		
 	}
 
 	/**
 	 * Cleans up the environment after running a test.
 	 */
-	protected function tearDown()
-	{
+	protected function tearDown() {
 		// TODO Auto-generated string_size_in_pixels_Test::tearDown()
-		
-		parent::tearDown();
+		parent::tearDown ();
 	}
 
 	/**
 	 * Constructs the test case.
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 		// TODO Auto-generated constructor
 	}
-
-	
 }
 
