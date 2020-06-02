e0d92d02989027227d5cbcaba9e61e7e14b68313
rudymartinb <paraespam541@gmail.com>
2020-06-01 22:25:24 -0300

    autocommit: adding diagram element()

--- src/diagram/element.php
+++ src/diagram/element.php
@@ -2,7 +2,15 @@
 namespace diagram;
 
 /*
- * element = class, interface or trait
+ * element = class, interface or trait on a diagram
+ * 
+ * just an rectangle with some text
+ * type should go first unless its a class
+ * name (with modifiers like abstract/final?)
+ * interfaces unless present on the diagram
+ * extends unless present on the diagram
+ * traits always for now.
+ * list of functions names (at least for now)
  */
 class element {
 	use element_properties;
