55bb11a145497467ca40d1bec9cc27cc9466e2e3
rudymartinb <paraespam541@gmail.com>
2020-06-03 18:40:48 -0300

    autocommit: adding VerticalLayout()

--- tests/diagram/doubles/draw_text_mock.php
+++ tests/diagram/doubles/draw_text_mock.php
@@ -5,16 +5,10 @@ use diagram\draw_text;
 class DrawTextMock extends draw_text {
 	
 	function __construct( string $text, int $width, int $height ) {
-		$this->text = $text;
+		$this->text = $text; // is this needed ?
 		$this->height_px = $height;
 		$this->width_px = $width;
 	}
 	
-// 	public function get_width(): int {
-// 	}
-
-// 	public function get_height(): int {
-// 	}
-
 	
 }
