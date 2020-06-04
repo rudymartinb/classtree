786aa27fe1b143ac29354ec70b24da8914c92f70
rudymartinb <paraespam541@gmail.com>
2020-06-03 23:25:57 -0300

    autocommit: adding VerticalLayout()

--- tests/diagram/vertical_layout_Test.php
+++ tests/diagram/vertical_layout_Test.php
@@ -21,10 +21,35 @@ class vertical_layout_Test extends PHPUnit\Framework\TestCase {
 		$this->assertEquals( 110, $layout->get_max_width() );
 		
 		
+		// just one element
+		// grid at 20,20
+		// margin 5
+		// equals 25 for x and y
 		$this->assertEquals( 25, $mytext->get_x() );
 		$this->assertEquals( 25, $mytext->get_y() );
+	}
+
+	
+	function test_2(){
+		$layout = new vertical_layout();
+		$layout->set_margin(5);
+		$layout->set_xy( 20,20 );
+		$mytext = new DrawTextMock("something goes here",  100, 20 );
+		$layout->add( $mytext );
+		$mytext = new DrawTextMock("shorter",  50, 20 );
+		$layout->add( $mytext );
+		$layout->do_layout();
+		$this->assertEquals( 2, $layout->get_num_components() );
+		$this->assertEquals( 50, $layout->get_max_height() );
+		$this->assertEquals( 110, $layout->get_max_width() );
 		
 		
+		// just one element
+		// grid at 20,20
+		// margin 5
+		// equals 25 for x and y
+		$this->assertEquals( 25, $mytext->get_x() );
+		$this->assertEquals( 45, $mytext->get_y() );
 	}
 	
 }
