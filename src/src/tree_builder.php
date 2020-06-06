d1983dd40440b5611758c40865e7100105e2e7cf
rudymartinb <paraespam541@gmail.com>
2020-06-06 00:22:34 -0300

    autocommit: adding tree_builder()

--- src/src/tree_builder.php
+++ src/src/tree_builder.php
@@ -3,7 +3,16 @@ namespace src;
 
 class tree_builder {
 
+	private $classes = [] ;
+	function add_source( string $source ){
+		$finder = new class_finder($source);
+		while( $finder->more_elements() ){
+			$class = [ "name" => $finder->get_name() ];
+			$this->classes[] = $class;
+			$finder->next();
+		}
+	}
 	function get_num_classes() : int {
-		return 0;
+		return count( $this->classes );
 	}
 }
--- tests/src/tree_builder_Test.php
+++ tests/src/tree_builder_Test.php
@@ -3,9 +3,17 @@
 use src\tree_builder;
 
 class tree_builder_Test extends PHPUnit\Framework\TestCase {
-	function test_nothing(){
+	function test_basic(){
 		$tree = new tree_builder();
 		$this->assertEquals(0, $tree->get_num_classes() );
+	}
+
+	function test_1_class(){
+		$source = 'class someclass {}';
+		
+		$tree = new tree_builder();
+		$tree->add_source( $source );
+		$this->assertEquals( 1, $tree->get_num_classes() );
 	}
 	
 
