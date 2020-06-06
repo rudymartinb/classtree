65c0737c65b2cc7a36081752e1d610180c0a5be8
rudymartinb <paraespam541@gmail.com>
2020-06-06 16:33:47 -0300

    autocommit: adding tree_builder()

--- src/src/class_tree_builder.php
+++ src/src/class_tree_builder.php
@@ -2,6 +2,35 @@
 namespace src;
 
 abstract class tree_builder {
+	protected $tree = [];
+	
+	function get_max_width(): int{
+		return $this->max_width($this->tree);
+	}
+	function get_max_height(): int{
+		return $this->tree[0]["height"];
+	}
+	
+	
+	protected function max_width( Array $trees ) : int {
+		$actual = 0;
+		foreach( $trees as $tree ){
+			$actual += $tree["width"];
+		}
+		return $actual;
+	}
+	
+	protected function max_height( Array $trees ) : int {
+		$maxheight = 0;
+		foreach( $trees as $tree ){
+			$actual = $tree["height"];
+			if( $actual > $maxheight ){
+				$maxheight = $actual;
+			}
+		}
+		return $maxheight;
+	}
+	
 	
 }
 /*
@@ -15,36 +44,9 @@ class class_tree_builder extends tree_builder {
 		return count( $this->classes );
 	}
 
-	function get_max_width(): int{
-		return $this->max_width($this->tree);
-	}
-	function get_max_height(): int{
-		return $this->tree[0]["height"];
-	}
+
 	
 	
-	private function max_width( Array $trees ) : int {
-		$actual = 0;
-		foreach( $trees as $tree ){
-			$actual += $tree["width"];
-		}
-		return $actual;
-	}
-	
-	private function max_height( Array $trees ) : int {
-		$maxheight = 0;
-		foreach( $trees as $tree ){
-			$actual = $tree["height"];
-			if( $actual > $maxheight ){
-				$maxheight = $actual;
-			}
-		}
-		return $maxheight;
-	}
-	
-	
-	
-	private $tree = [];
 	function resolve_class_hierarchy( string $parent = "" ) {
 		$this->tree = $this->resolve();
 	}
