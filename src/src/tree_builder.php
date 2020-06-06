f46f1f0efb2d4c599c536aa7b563d1c1415a7cc0
rudymartinb <paraespam541@gmail.com>
2020-06-06 00:23:17 -0300

    autocommit: adding tree_builder()

--- src/src/tree_builder.php
+++ src/src/tree_builder.php
@@ -7,7 +7,8 @@ class tree_builder {
 	function add_source( string $source ){
 		$finder = new class_finder($source);
 		while( $finder->more_elements() ){
-			$class = [ "name" => $finder->get_name() ];
+			$class = [];
+			$class[] = [ "name" => $finder->get_name() ];
 			$this->classes[] = $class;
 			$finder->next();
 		}
