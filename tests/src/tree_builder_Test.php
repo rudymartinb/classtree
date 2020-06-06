6bf43c7c733c601c569d742b36181e2d721173d2
rudymartinb <paraespam541@gmail.com>
2020-06-06 01:11:23 -0300

    autocommit: adding tree_builder()

--- tests/src/tree_builder_Test.php
+++ tests/src/tree_builder_Test.php
@@ -8,17 +8,27 @@ class tree_builder_Test extends PHPUnit\Framework\TestCase {
 		$this->assertEquals(0, $tree->get_num_classes() );
 	}
 
-	function test_1_class(){
+	function test_search_class(){
 		$source = 'class someclass {
 }';
 		
 		$tree = new tree_builder();
 		$tree->add_source( $source );
-		$this->assertEquals( 1, $tree->get_num_classes() );
-		
 		$this->assertEquals( 0, $tree->search_class("someclass") );
 		$this->assertEquals( -1, $tree->search_class("does_not_exist") );
 	}
+
+	function test_namespace(){
+		$source = 'namespace src;
+class someclass {
+}';
+		
+		$tree = new tree_builder();
+		$tree->add_source( $source );
+		$this->assertEquals( 0, $tree->search_class("someclass") );
+		$this->assertEquals( -1, $tree->search_class("does_not_exist") );
+	}
+	
 	
 
 }
