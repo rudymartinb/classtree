72cf6023031af62d7694e725cf32bf62272f00a7
rudymartinb <paraespam541@gmail.com>
2020-06-06 20:04:50 -0300

    autocommit: adding function return values to class_finder()

--- tests/src/class_finder_Test.php
+++ tests/src/class_finder_Test.php
@@ -209,23 +209,10 @@ function something2( int $ant, string $strong );
     	$finder = new class_finder( $source );
     	
     	// function something1
-    	$this->assertEquals( true, $finder->more_elements() );
-    	
     	$this->assertEquals( true, $finder->more_traits() );
     	$this->assertEquals( "sometrait", $finder->get_trait_name() );
     	$finder->next_trait();
     	$this->assertEquals( "sometrait2", $finder->get_trait_name() );
-    	
-    	
-    	$this->assertEquals( true, $finder->more_functions() );
-    	$this->assertEquals( "something1", $finder->get_function_name() );
-    	$this->assertEquals( false, $finder->more_parameters() );
-    	$finder->next_function();
-    	$this->assertEquals( "something2", $finder->get_function_name() );
-    	$finder->next_parameter();
-    	$this->assertEquals( true, $finder->more_parameters() );
-    	$this->assertEquals( "int", $finder->get_parameter_type() );
-    	$this->assertEquals( "ant", $finder->get_parameter_name() );
     	
     	
     	
