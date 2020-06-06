e22a6e76556a566771df4ff8518123379b2f2532
rudymartinb <paraespam541@gmail.com>
2020-06-06 00:15:34 -0300

    autocommit: cleanup and  adding diagram element()

--- tests/src/tree_builder_Test.php
+++ tests/src/tree_builder_Test.php
@@ -1,32 +1,6 @@
 <?php
 
-/**
- *  test case.
- */
-class tree_builder_Test extends PHPUnit_Framework_TestCase {
+class tree_builder_Test extends PHPUnit\Framework\TestCase {
+	
 
-	/**
-	 * Prepares the environment before running a test.
-	 */
-	protected function setUp() {
-		parent::setUp ();
-
-		// TODO Auto-generated tree_builder_Test::setUp()
-	}
-
-	/**
-	 * Cleans up the environment after running a test.
-	 */
-	protected function tearDown() {
-		// TODO Auto-generated tree_builder_Test::tearDown()
-		parent::tearDown ();
-	}
-
-	/**
-	 * Constructs the test case.
-	 */
-	public function __construct() {
-		// TODO Auto-generated constructor
-	}
 }
-
