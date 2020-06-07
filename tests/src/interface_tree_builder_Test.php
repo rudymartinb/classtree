6908ba25d456f131ba6e104abceb4c9b8e8365d0
rudymartinb <paraespam541@gmail.com>
2020-06-07 12:42:43 -0300

    autocommit: class and interface tree_builder()

--- tests/src/interface_tree_builder_Test.php
+++ tests/src/interface_tree_builder_Test.php
@@ -1,5 +1,7 @@
 <?php
 
+use src\interface_tree_builder;
+
 class interface_tree_builder_Test extends PHPUnit\Framework\TestCase {
 	function test_basic(){
 		$builder = new interface_tree_builder();
