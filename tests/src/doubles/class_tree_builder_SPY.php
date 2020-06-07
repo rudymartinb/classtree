c1288fca3a7e833f3aae6d3dc3ac136103295457
rudymartinb <paraespam541@gmail.com>
2020-06-07 00:07:29 -0300

    autocommit: adding tree_builder()

--- tests/src/doubles/class_tree_builder_SPY.php
+++ tests/src/doubles/class_tree_builder_SPY.php
@@ -1,14 +1,16 @@
 <?php 
 use src\class_tree_builder;
+use src\class_collector;
 
 class class_tree_builder_SPY extends class_tree_builder {
+	function get_collector() : class_collector {
+		return $this->collector;
+	}
+	
 	function get_num_classes() : int {
 		return $this->collector->get_count();
 	}
 	
-// 	function get_namespace() : string {
-// 		return $this->classes[ $this->class_index ]["namespace"];
-// 	}
 	
 // 	protected $class_index = null;
 // 	protected $function_index = null;
