c790979ac300239da315d0cda467950627ffbbc1
rudymartinb <paraespam541@gmail.com>
2020-06-07 13:06:20 -0300

    autocommit: class and interface tree_builder()

--- tests/src/interface_tree_builder_Test.php
+++ tests/src/interface_tree_builder_Test.php
@@ -34,5 +34,18 @@ class interface_tree_builder_Test extends PHPUnit\Framework\TestCase {
 		$this->assertEquals( 1, $tree->get_max_height() );
 	}
 	
+	function test_size_3(){
+		$tree = new interface_tree_builder_SPY();
+		$tree->add_source( 'interface someinterface {}' );
+		$tree->add_source( 'interface someinterface2 extends someinterface {} ' );
+		
+		$tree->resolve_hierarchy();
+		
+		$this->assertEquals( 1, $tree->get_max_width() );
+		$this->assertEquals( 2, $tree->get_max_height() );
+	}
+	
+	
+	
 }
 
