2be2fdfbc3ca7768bca4dd5df91be2b4824c42be
rudymartinb <paraespam541@gmail.com>
2020-06-07 00:39:40 -0300

    autocommit: adding tree_builder()

--- src/src/class_collector.php
+++ src/src/class_collector.php
@@ -1,6 +1,14 @@
 <?php
 namespace src;
 abstract class collector {
+	protected $data = [];
+	function more_elements() : bool {
+		return count( $this->data ) > $this->current_key;
+	}
+	function get() : Array {
+		return $this->data[$this->current_key];
+	}
+	
 	protected $current_key = 0;
 	function next(){
 		$this->current_key ++;
@@ -15,14 +23,6 @@ class class_collector extends collector {
 		}
 	}
 	
-	private $data = [];
-	
-	function more_elements() : bool {
-		return count( $this->data ) > $this->current_key;
-	}
-	function get() : Array {
-		return $this->data[$this->current_key];
-	}
 	function get_name() : string {
 		return $this->data[$this->current_key]["name"];
 	}
