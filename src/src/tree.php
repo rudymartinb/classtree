4b387e42d53380e97e554258dbaac6cd97b091a2
rudymartinb <paraespam541@gmail.com>
2020-06-08 19:45:29 -0300

    autocommit: refactoring class_tree_builder()

--- src/src/tree.php
+++ src/src/tree.php
@@ -1,18 +1,56 @@
 <?php
 namespace tree;
+
+interface tree_interface {
+	function set_name( string $name );
+	function set_namespace( string $name );
+	function set_children( Array $tree );
+	function set_width( int $width ) ;
+	function set_height( int $height );
+	
+	function get_name(): string;
+	function get_namespace(): string;
+	function get_children(): Array;
+	function get_width(): int;
+	function get_height(): int;
+}
+
 /* 
  * I need a class to hold the info of just one tree of classes
  * I dont care if it just hold one class only
  * 
  * main objective: avoid having to walk into an array of arrays
  */
-class tree {
+class tree implements tree_interface {
 	private $data;
 	function __construct( Array $data_tree ){
 		$this->data = $data_tree;
 	}
-	function get_width(){
+	function get_width() : int {
 		return count( $this->data );
 	}
+	function set_width( int $width ) {
+	}
+	
+	function set_name(string $name) {
+	}
+	function get_name(): string {
+	}
+	
+	function set_namespace(string $name) {
+	}
+	function get_namespace(): string {
+	}
+
+	function set_children(array $tree) {
+	}
+	function get_children(): array {
+	}
+
+	function set_height(int $height) {
+	}
+	function get_height(): int {
+	}
+
 	
 }
