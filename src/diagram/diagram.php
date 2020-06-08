dc8bcb1d4e4f72dd9859ac46c1ecae2af6197788
rudymartinb <paraespam541@gmail.com>
2020-06-08 16:35:23 -0300

    autocommit: refactoring class_tree_builder()

--- src/diagram/diagram.php
+++ src/diagram/diagram.php
@@ -1,82 +1,84 @@
 <?php
 namespace diagram;
 use src\interface_finder;
+use src\class_tree_builder;
 
 class diagram {
-	function get_num_elements() : int {
-		return count( $this->elements );
-	}
 	
-	private $elements = [];
+	private $class_tree;
+	function __construct(){
+		$this->class_tree = new class_tree_builder();
+	}
 	function add_source( string $source ){
-		$finder = new interface_finder($source);
-		while( $finder->more_elements() ){
-			$element = new element();
-			$element->set_name($finder->get_name());
-			$element->set_namespace($finder->get_namespace());
-			$this->elements[] = $element;
-			$finder->next();
-		}
+		
+		$this->class_tree->add_source($source);
+		$this->class_tree->resolve_hierarchy();
+	}
+	function get_width() : int {
+		return $this->class_tree->get_max_width();
+	}
+	function get_height() : int {
+		return $this->class_tree->get_max_height();
 	}
 	
-	function get_element_by_name( string $name ) : element {
-		foreach( $this->elements as $element ){
-			if( $element->get_name() === $name ){
-				return $element;
-			}
-		}
-		// this should throw an exception
-		return null;
-	}
-	private $current_key = 0;
-	function place(){
-		$element = $this->elements[ $this->current_key ];
-		$element->set_position( 0, 0 );
+// 	function get_element_by_name( string $name ) : element {
+// 		foreach( $this->elements as $element ){
+// 			if( $element->get_name() === $name ){
+// 				return $element;
+// 			}
+// 		}
+// 		// this should throw an exception
+// 		return null;
+// 	}
+// 	private $current_key = 0;
+// 	function place(){
+// 		$element = $this->elements[ $this->current_key ];
+// 		$element->set_position( 0, 0 );
 		
-	}
+// 	}
 	
-	function draw(){
+// 	function draw(){
 		
 		
-		$this->maxwidth = 1024;
-		$this->maxheight = 768;
+// 		$this->maxwidth = 1024;
+// 		$this->maxheight = 768;
 		
-		$this->img = imagecreatetruecolor( $this->maxwidth, $this->maxheight);
-		imageantialias ( $this->img, true );
+// 		$this->img = imagecreatetruecolor( $this->maxwidth, $this->maxheight);
+// 		imageantialias ( $this->img, true );
 		
-		/* background color
-		 */
-		$this->color["white"] = imagecolorallocate($this->img, 255,   255,  255);
+// 		/* background color
+// 		 */
+// 		$this->color["white"] = imagecolorallocate($this->img, 255,   255,  255);
 		
-		/* boxes
-		 */
-		$this->color["black"] = imagecolorallocate($this->img, 0,   0,  0);
+// 		/* boxes
+// 		 */
+// 		$this->color["black"] = imagecolorallocate($this->img, 0,   0,  0);
 		
-		$this->color["gray"]   = imagecolorallocate($this->img, 240,   240,  240);
+// 		$this->color["gray"]   = imagecolorallocate($this->img, 240,   240,  240);
 		
 		
-		/* canvas
-		 */
-		imagefilledrectangle($this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
+// 		/* canvas
+// 		 */
+// 		imagefilledrectangle($this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
 		
-	}
+// 	}
 	
-	/* TODO: calculate heigh based on the number of functions
-	 */
-	private function draw_class( string $name ){
-		$x = 10;
-		$y = 10;
-		$width = 100;
-		$height = 50;
-		imagefilledrectangle($this->img, $x, $y, $x+$width, $y+$height, $this->color["white"] );
-		imagerectangle($this->img, $x, $y, $x+$width, $y+$height, $this->color["black"] );
+// 	/* TODO: calculate heigh based on the number of functions
+// 	 */
+// 	private function draw_class( string $name ){
+// 		$x = 10;
+// 		$y = 10;
+// 		$width = 100;
+// 		$height = 50;
+// 		imagefilledrectangle($this->img, $x, $y, $x+$width, $y+$height, $this->color["white"] );
+// 		imagerectangle($this->img, $x, $y, $x+$width, $y+$height, $this->color["black"] );
 		
 		
-		$font = './fonts/courier.ttf';
-		$font = realpath($font) ;
-		$text = $name;
-		\imagettftext($this->img, 10,0.0, $x+5, $y+15, $this->color["black"] , $font, $text);
-	}
+// 		$font = './fonts/courier.ttf';
+// 		$font = realpath($font) ;
+// 		$text = $name;
+// 		\imagettftext($this->img, 10,0.0, $x+5, $y+15, $this->color["black"] , $font, $text);
+// 	}
 	
 	
 }
--- tests/diagram/diagram_Test.php
+++ tests/diagram/diagram_Test.php
@@ -5,27 +5,24 @@ use diagram\diagram;
 class diagram_Test extends PHPUnit\Framework\TestCase {
 	function test_empty(){
 		$diagram = new diagram();
-		$this->assertEquals(0, $diagram->get_num_elements() );
+		$this->assertEquals(0, $diagram->get_width() );
 	}
 	
 	function test_add_source(){
 		$source = "";
 		$diagram = new diagram();
 		$diagram->add_source($source);
-		$this->assertEquals(0, $diagram->get_num_elements() );
+		$this->assertEquals(0, $diagram->get_width() );
 	}
 
 	function test_class_getname(){
 		$source = "class myclass {}";
 		$diagram = new diagram();
 		$diagram->add_source($source);
-		$this->assertEquals(1, $diagram->get_num_elements() );
 		
 		$this->assertEquals(1, $diagram->get_width() );
 		$this->assertEquals(1, $diagram->get_height() );
 
-		$element = $diagram->get_element_by_name( "myclass" );
-		$this->assertEquals("myclass", $element->get_name() );
 	}
 
 	
