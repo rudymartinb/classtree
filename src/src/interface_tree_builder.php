3f6ddf55feaaf3b2148e98e0051cb45de0dac7d5
rudymartinb <paraespam541@gmail.com>
2020-06-08 21:36:58 -0300

    autocommit: refactoring class_tree_builder()

--- src/src/interface_tree_builder.php
+++ src/src/interface_tree_builder.php
@@ -5,16 +5,11 @@ use tree\tree;
 
 class interface_tree_builder extends tree_builder {
 	
-	private $newcollector;
 	function __construct(){
 		$this->collector = new interface_collector();
-		$this->newcollector = function(){
-			return new interface_collector( $this->collector );
-		};
 	}
 	
 	function get_new_collector() : collector {
-// 		$func = $this->newcollector;
 		return new interface_collector( $this->collector );
 	}
  
