e8f879b07fc45e215a8215a419bd462cd29fd1d5
rudymartinb <paraespam541@gmail.com>
2020-06-08 21:35:46 -0300

    autocommit: refactoring class_tree_builder()

--- src/src/class_tree_builder.php
+++ src/src/class_tree_builder.php
@@ -17,43 +17,6 @@ class class_tree_builder extends tree_builder {
 		$func = $this->newcollector;
 		return $func();
 	}
-	
-
-	protected function resolve( string $parent = "" ) : Array {
-		// by doing this we keep the internal pointer
-		// separated on each recursive call.
-		$collector = new class_collector( $this->collector );
-		
-		$tree = [];
-		while( $collector->more_elements() ){
-			$classname = $collector->get_name();
-			$extends = $collector->get_extends();
-			
-			if( $parent !== "" ){
-				if( $extends != $parent ) {
-					$collector->next();
-					continue;
-				}
-			} else {
-				if( $extends !== "" ){
-					$collector->next();
-					continue;
-				}
-			}
-
-			$children = $this->resolve( $classname );
-			
-			$node = new tree( $classname );
-			$node->set_extends($extends);
-			$node->set_children($children);
-			$node->set_width( max( $this->max_width( $children ), 1 ) );
-			$node->set_height( $this->max_height( $children )+1 );
-			$tree[] = $node;
-					
-			$collector->next();
-		}
-		return $tree;
-	}
 
 	
 }
