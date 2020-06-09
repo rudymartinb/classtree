f215b423de112ca52623e7297b0a890c2d5ff639
rudymartinb <paraespam541@gmail.com>
2020-06-08 21:38:53 -0300

    autocommit: refactoring class_tree_builder()

--- src/src/class_tree_builder.php
+++ src/src/class_tree_builder.php
@@ -3,8 +3,12 @@ namespace src;
 
 use tree\tree;
 
+/* now
+ * we could entirely make this file disappear 
+ * by passing the collector class to the constructor
+ * and change the base class to a normal class.
+ */
 class class_tree_builder extends tree_builder {
-
 	
 	function __construct(){
 		$this->collector = new class_collector();
