c24b2b37d7f2e4834138c436b7a3fe08331a9e8d
rudymartinb <paraespam541@gmail.com>
2020-06-10 00:58:04 -0300

    autocommit: adding diagram tree columns

--- src/src/tree.php
+++ src/src/tree.php
@@ -59,6 +59,15 @@ class tree {
 		return $this->height;
 	}
 
+	/*
+	 * these relative coordinates reffers to a virtual grid from X,Y 
+	 * where 0,0 would be the upper left corner of the screen.
+	 * the idea is to multiply these values per the maximum height and width of all elements
+	 * plus some extra space among them (50% of height, 100% width)
+	 * to make sure they don't overlap
+	 * 
+	 * that will give us the propper coordinates of the screen. 
+	 */
 	private $relcol;
 	function set_relcol( int $relcol ) {
 		$this->relcol = $relcol;
