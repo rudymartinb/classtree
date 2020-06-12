33ef35dbded510990d4d86aceef426f834569c96
rudymartinb <paraespam541@gmail.com>
2020-06-12 00:45:25 -0300

    autocommit: drawing and refactoring

--- src/src/class_tree_builder.php
+++ src/src/class_tree_builder.php
@@ -1,6 +1,8 @@
 <?php
 namespace src;
 
+use diagram\vertical_layout;
+
 class class_tree_builder extends tree_builder {
 	
 	function __construct(){
@@ -11,5 +13,16 @@ class class_tree_builder extends tree_builder {
 		return new class_collector( $this->collector );
 	}
 
+	function draw() {
+
+		$layout = new vertical_layout();
+		$layout->set_margin(5);
+		$layout->add_text( "something goes here" );
+		$layout->do_layout();
+		$layout->set_xy( $layout->get_max_width() /2, $layout->get_max_height() /2 );
+		$layout->do_layout();
+		$layout->draw( $this->img );
+		
+	}
 	
 }
