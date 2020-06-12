2a9417992f90a7775ac326eea9ab6a55dc432a2e
rudymartinb <paraespam541@gmail.com>
2020-06-12 00:44:10 -0300

    autocommit: drawing and refactoring

--- src/diagram/diagram.php
+++ src/diagram/diagram.php
@@ -41,65 +41,21 @@ class diagram {
 		 */
 		$this->img = imagecreatetruecolor( $this->maxwidth  , $this->maxheight );
 		imagefilledrectangle( $this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
+		imageantialias ( $this->img, true );
 		
-		$this->class_tree->draw( $img );
+		$this->class_tree->draw( $this->img );
 		$layout = new vertical_layout();
 		$layout->set_margin(5);
 		$layout->add_text( "something goes here" );
 		$layout->do_layout();
 		$layout->set_xy( $layout->get_max_width() /2, $layout->get_max_height() /2 );
-		
-		
-		
-		imageantialias ( $this->img, true );
-		
 		$layout->do_layout();
 		$layout->draw( $this->img );
 		
-		// 		$this->assertEquals( 25, $mytext->get_x() );
-		// 		$this->assertEquals( 48, $mytext->get_y() );
-		
 		\imagepng($this->img,"/var/www/htdocs/salida.png");
-		
-// 		$this->maxwidth = $this->get_width();
-// 		$this->maxheight = 768;
-		
-// 		$this->img = imagecreatetruecolor( $this->maxwidth, $this->maxheight);
-// 		imageantialias ( $this->img, true );
-		
-// 		/* background color
-// 		 */
-// 		$this->color["white"] = imagecolorallocate($this->img, 255,   255,  255);
-		
-// 		/* boxes
-// 		 */
-// 		$this->color["black"] = imagecolorallocate($this->img, 0,   0,  0);
-		
-// 		$this->color["gray"]   = imagecolorallocate($this->img, 240,   240,  240);
-		
-		
-// 		/* canvas
-// 		 */
-// 		imagefilledrectangle($this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
 		
 	}
 	
-// 	/* TODO: calculate heigh based on the number of functions
-// 	 */
-// 	private function draw_class( string $name ){
-// 		$x = 10;
-// 		$y = 10;
-// 		$width = 100;
-// 		$height = 50;
-// 		imagefilledrectangle($this->img, $x, $y, $x+$width, $y+$height, $this->color["white"] );
-// 		imagerectangle($this->img, $x, $y, $x+$width, $y+$height, $this->color["black"] );
-		
-		
-// 		$font = './fonts/courier.ttf';
-// 		$font = realpath($font) ;
-// 		$text = $name;
-// 		\imagettftext($this->img, 10,0.0, $x+5, $y+15, $this->color["black"] , $font, $text);
-// 	}
 	
 	
 }
