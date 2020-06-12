7a783cc884c6624c1a6a3e4c46f15d6a2a68daaa
rudymartinb <paraespam541@gmail.com>
2020-06-11 23:59:33 -0300

    autocommit: adding relative position calculatitions

--- tests/diagram/vertical_layout_Test.php
+++ tests/diagram/vertical_layout_Test.php
@@ -22,6 +22,17 @@ class vertical_layout_Test extends PHPUnit\Framework\TestCase {
 		$layout->set_xy( $layout->get_max_width() /2, $layout->get_max_height() /2 );
 
 		$this->img = imagecreatetruecolor( $layout->get_max_width() *2, $layout->get_max_height() * 2 );
+		
+		/* background color
+		 */
+		$this->color["white"] = imagecolorallocate($this->img, 255,   255,  255);
+		$this->color["black"] = imagecolorallocate($this->img, 0,   0,  0);
+		$this->color["gray" ] = imagecolorallocate($this->img, 240,   240,  240);
+		
+		/* canvas
+		 */
+		imagefilledrectangle($this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
+		
 		imageantialias ( $this->img, true );
 		$layout->draw($this->img );
 		
