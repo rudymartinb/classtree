25f40a38fbeea36e1ec45a23a487f4bf90a80969
rudymartinb <paraespam541@gmail.com>
2020-06-12 00:39:06 -0300

    autocommit: drawing and refactoring

--- src/diagram/diagram.php
+++ src/diagram/diagram.php
@@ -43,27 +43,61 @@ class diagram {
 	
 	function draw(){
 		
+		$layout = new vertical_layout();
+		$layout->set_margin(5);
 		
-		$this->maxwidth = $this->get_width();
-		$this->maxheight = 768;
+		$layout->add_text( "something goes here" );
 		
-		$this->img = imagecreatetruecolor( $this->maxwidth, $this->maxheight);
-		imageantialias ( $this->img, true );
+		$layout->do_layout();
+		$this->assertEquals( 1, $layout->get_num_components() );
+		// 		$this->assertEquals( 26, $layout->get_max_height() );
+		// 		$this->assertEquals( 162, $layout->get_max_width() );
+		$layout->set_xy( $layout->get_max_width() /2, $layout->get_max_height() /2 );
+		
+		$this->maxwidth = $layout->get_max_width() *2;
+		$this->maxheight = $layout->get_max_height() * 2;
+		$this->img = imagecreatetruecolor( $this->maxwidth  , $this->maxheight );
 		
 		/* background color
 		 */
 		$this->color["white"] = imagecolorallocate($this->img, 255,   255,  255);
-		
-		/* boxes
-		 */
 		$this->color["black"] = imagecolorallocate($this->img, 0,   0,  0);
-		
-		$this->color["gray"]   = imagecolorallocate($this->img, 240,   240,  240);
-		
+		$this->color["gray" ] = imagecolorallocate($this->img, 240,   240,  240);
 		
 		/* canvas
 		 */
-		imagefilledrectangle($this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
+		imagefilledrectangle( $this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
+		
+		imageantialias ( $this->img, true );
+		
+		$layout->do_layout();
+		$layout->draw( $this->img );
+		
+		// 		$this->assertEquals( 25, $mytext->get_x() );
+		// 		$this->assertEquals( 48, $mytext->get_y() );
+		
+		\imagepng($this->img,"/var/www/htdocs/salida.png");
+		
+// 		$this->maxwidth = $this->get_width();
+// 		$this->maxheight = 768;
+		
+// 		$this->img = imagecreatetruecolor( $this->maxwidth, $this->maxheight);
+// 		imageantialias ( $this->img, true );
+		
+// 		/* background color
+// 		 */
+// 		$this->color["white"] = imagecolorallocate($this->img, 255,   255,  255);
+		
+// 		/* boxes
+// 		 */
+// 		$this->color["black"] = imagecolorallocate($this->img, 0,   0,  0);
+		
+// 		$this->color["gray"]   = imagecolorallocate($this->img, 240,   240,  240);
+		
+		
+// 		/* canvas
+// 		 */
+// 		imagefilledrectangle($this->img, 0,0,$this->maxwidth-1, $this->maxheight-1, $this->color["white"]);
 		
 	}
 	
