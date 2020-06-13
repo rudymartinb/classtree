ff6bacca91c1162a217c97878a55e0ea5bed2d03
rudymartinb <paraespam541@gmail.com>
2020-06-13 00:22:21 -0300

    autocommit: drawing and refactoring

--- src/src/tree_builder.php
+++ src/src/tree_builder.php
@@ -130,7 +130,7 @@ abstract class tree_builder {
 	}
 	
 	
-	private $width_margin = 1.2;
+	private $width_margin = 2;
 	private $height_margin = 3;
 	private $max_img_width;
 	private $max_img_height;
