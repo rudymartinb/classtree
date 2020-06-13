745d75c4824b585bd6df6c1538f76ce7d3d3ba2d
rudymartinb <paraespam541@gmail.com>
2020-06-12 22:29:29 -0300

    autocommit: drawing and refactoring

--- tests/diagram/diagram_Test.php
+++ tests/diagram/diagram_Test.php
@@ -29,38 +29,6 @@ class diagram_Test extends PHPUnit\Framework\TestCase {
 
 	}
 	
-// 	function test_build_element(){
-// 		$source = "class myclass {}";
-// 		$diagram = new diagram();
-// 		$diagram->add_source($source);
-		
-// 	}
-
-	
-// 	function test_class_getname(){
-// 		$source = "class myclass {}";
-// 		$diagram = new diagram();
-// 		$diagram->add_source($source);
-// 		$this->assertEquals(1, $diagram->get_num_elements() );
-		
-// 		$element = $diagram->get_element_by_name( "myclass" );
-// 		$this->assertEquals("myclass", $element->get_name() );
-// 	}
-
-// 	function test_class_place(){
-// 		$source = "class myclass {}";
-// 		$diagram = new diagram();
-// 		$diagram->add_source($source);
-		
-// 		$diagram->place();
-		
-// 		$element = $diagram->get_element_by_name( "myclass" );
-		
-// 		$this->assertEquals( true, $element->is_placed() );
-		
-// // 		$diagram->draw();
-		
-// 	}
 	
 }
 
