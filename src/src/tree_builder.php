d663e0ee2d19131e86c166588d48197fa27a823e
rudymartinb <paraespam541@gmail.com>
2020-06-13 00:50:16 -0300

    autocommit: drawing and refactoring

--- src/src/tree_builder.php
+++ src/src/tree_builder.php
@@ -195,7 +195,7 @@ abstract class tree_builder {
 		$layout->add_text( $node->get_name() );
 		$layout->do_layout();
 		$x = ( ( $node->get_relcol()+(($node->get_width()-1)/2)+0.25 ) * $this->max_node_width_px  * $this->width_margin  )  ;
-		$y = ( ( $node->get_relrow()  ) * $this->max_node_height_px * $this->height_margin ) ;
+		$y = ( ( $node->get_relrow()+0.25  ) * $this->max_node_height_px * $this->height_margin ) ;
 		
 		$layout->set_xy( $x, $y );
 		
