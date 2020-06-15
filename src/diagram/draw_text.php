2916c931eb90d35f2a88dafa2a9cdd93fb50e30d
rudymartinb <paraespam541@gmail.com>
2020-06-14 23:27:42 -0300

    autocommit: refactoring tree_builder

--- src/diagram/draw_text.php
+++ src/diagram/draw_text.php
@@ -50,7 +50,6 @@ class draw_text implements component {
 	
 	function __construct( string $text  ){
 		$this->font_size = 10;
-		
 		$size = $this->font_size;
 		$this->draw_function = function( $img, $font ) use( $size ){
 			\imagettftext($img, $size,0.0, $this->x, $this->y, $this->color["black"] , $font, $this->text);
