a8c10f618617dc6135b5ebd5ce6946e349d9523f
rudymartinb <paraespam541@gmail.com>
2020-06-14 23:15:19 -0300

    autocommit: refactoring tree_builder

--- src/src/tree_builder.php
+++ src/src/tree_builder.php
@@ -392,7 +392,7 @@ abstract class tree_builder {
 			imagefilledpolygon($this->img, $points, 3, $this->color["white"]);
 			imagepolygon($this->img, $points, 3, $this->color["black"]);
 		} else {
-			$points = array( $x10, $y10 , $xx1, $yy1 , $xx2, $yy2 );
+			$points = array( $x1, $y1 , $xx1, $yy1 , $xx2, $yy2 );
 			imagepolygon($this->img, $points, 2, $this->color["black"]);
 			
 			imageline($this->img, $x1, $y1 , $xx2, $yy2, $this->color["black"]);
